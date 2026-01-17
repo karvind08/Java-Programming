2010747cebd9446587070768026e6d206c288704
karvind08 <karvind08@gmail.com>
2026-01-17 21:21:55 +0530

    17 January 2026

---
+++ Unit 01/Package/Demo.java
@@ -0,0 +1,9 @@
+import pack1.*;
+
+class Demo {
+    public static void main(String args[]) {
+        A obj = new A();
+        int rs = obj.add(10, 20);
+        System.out.println("Result: " + rs);
+    }
+}
---
+++ Unit 01/Package/Pack1/A.java
@@ -0,0 +1,6 @@
+public class A {
+    public int add(int a1, int b1) {
+        int r1 = a1 + b1;
+        return r1;
+    }
+}
