59336a3b20e7caa9eb607546d1523f5928e5bf45
karvind08 <karvind08@gmail.com>
2026-01-29 22:25:46 +0530

    29 January 2026

---
+++ Unit 01/Default2.java
@@ -0,0 +1,13 @@
+public class Default2 {
+    public static void main(String[] args) {
+        int a = 2;
+        float b = 0.6f;
+        double d = 4.56d;
+        long l = 19;
+        System.out.println("int value: " + a);
+        System.out.println("float value: " + b);
+        System.out.println("double value: " + d);
+        System.out.println("long value: " + l);
+    }
+
+}
