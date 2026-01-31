d39eb90b7717352eb5c78cdc9f76790667debc3f
karvind08 <karvind08@gmail.com>
2026-01-31 15:15:00 +0530

    31 January 2026

--- Unit 01/Type Promotion/Promote.java
+++ Unit 01/Type Promotion/Promote.java
@@ -1,3 +1,6 @@
 public class Promote {
-
+    public static void main(String[] args) {
+        TypePromot1 TP = new TypePromot1();
+        TP.add(2, 3);
+    }
 }
--- Unit 01/Type Promotion/TypePromot1.java
+++ Unit 01/Type Promotion/TypePromot1.java
@@ -1,3 +1,19 @@
 public class TypePromot1 {
-    
+    // void add(int a, int b) {
+    // System.out.println("The sum is " + (a + b));
+    // }
+
+    void add(int a, int b, int c) {
+        System.out.println("The sum is " + (a + b + c));
+    }
+
+    /* This is creating ambiguity */
+    // void add(long a, int b) {
+    // System.out.println("The sum is " + (a + b));
+    // }
+
+    void add(int a, long b) {
+        System.out.println("The sum is " + (a + b));
+    }
+
 }
