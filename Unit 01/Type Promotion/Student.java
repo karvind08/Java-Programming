b3e5da7ef703a06688f8c49eee441dc6e9ec0285
karvind08 <karvind08@gmail.com>
2026-01-31 18:51:02 +0530

    31 January 2026

--- Unit 01/Type Promotion/Student.java
+++ Unit 01/Type Promotion/Student.java
@@ -11,20 +11,9 @@ public class Student {
     }
 
     void display() {
-        System.out
-                .println(
-                        "Name: " + name + " Roll: " + roll + " College Name: " + collName + " Student Count: "
-                                + stcount);
-    }
-
-    public static void main(String[] args) {
-        Student S1 = new Student();
-        S1.getdata("Arvind", 43);
-        S1.display();
-        System.out.println("\nThe total students are: " + Student.stcount);
-        Student S2 = new Student();
-        S2.getdata("Yuvaan", 5);
-        S2.display();
-        System.out.println("\nThe total students are: " + Student.stcount);
+        System.out.println("Name: " + name);
+        System.out.println("Roll: " + roll);
+        System.out.println("College Name: " + collName);
+        System.out.println("Student Count: " + stcount);
     }
 }
