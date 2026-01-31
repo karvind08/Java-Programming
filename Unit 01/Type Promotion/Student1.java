3abd93ad4a4fb52498375839063cbbecc1aadabf
karvind08 <karvind08@gmail.com>
2026-01-31 19:00:23 +0530

    31 January 2026

---
+++ Unit 01/Type Promotion/Student1.java
@@ -0,0 +1,19 @@
+public class Student1 {
+    String name;
+    int roll;
+    static String collName = "MMMEC";
+    static int stcount = 0;
+
+    void Student1(String n, int r) {
+        this.name = n;
+        this.roll = r;
+        stcount++;
+    }
+
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Roll: " + roll);
+        System.out.println("College Name: " + collName);
+        System.out.println("Student Count: " + stcount);
+    }
+}
