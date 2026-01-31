5e98e1d5e75a53ef59ecb4c551ebbaf3d9e109ab
karvind08 <karvind08@gmail.com>
2026-01-31 18:51:24 +0530

    31 January 2026

--- Unit 01/Type Promotion/StudentDB.java
+++ Unit 01/Type Promotion/StudentDB.java
@@ -1,3 +1,12 @@
 public class StudentDB {
-    
+    public static void main(String[] args) {
+        Student S1 = new Student();
+        S1.getdata("Arvind", 43);
+        S1.display();
+        System.out.println("\nThe total students are: " + Student.stcount);
+        Student S2 = new Student();
+        S2.getdata("Yuvaan", 5);
+        S2.display();
+        System.out.println("\nThe total students are: " + Student.stcount);
+    }
 }
