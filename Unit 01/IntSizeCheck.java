95ffab590725e1f2c7faac23d7ff57c1285a5311
karvind08 <karvind08@gmail.com>
2026-02-03 22:18:05 +0530

    3 February 2026

--- Unit 01/IntSizeCheck.java
+++ Unit 01/IntSizeCheck.java
@@ -1,5 +1,7 @@
 public class IntSizeCheck {
     public static void main(String[] args) {
-
+        System.out.println("The size of int in Bits: " + Integer.SIZE);
+        System.out.println("The size of int in Bytes: " + Integer.BYTES);
+        System.out.println("The min value of int: " + Integer.MIN_VALUE);
     }
 }
