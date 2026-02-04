60e16e2007adaccfc64930629dffcbeb4363a125
karvind08 <karvind08@gmail.com>
2026-02-04 23:42:33 +0530

    4 February 2026

--- Unit 01/Example.java
+++ Unit 01/Example.java
@@ -20,10 +20,16 @@ class Static {
 
 public class Example {
     public static void main(String[] args) {
+        System.out.println("The college: " + Static.College);
+        System.out.println("Object: " + Static.count);
         Static S1 = new Static();
         S1.getdata(1, "Arvind");
         S1.display();
+        Static S2 = new Static();
         System.out.println("The college: " + Static.College);
         System.out.println("Object: " + Static.count);
+        S2.getdata(2, "Yuvan");
+        S2.display();
+
     }
 }
