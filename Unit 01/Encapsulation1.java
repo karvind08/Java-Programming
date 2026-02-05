1a0e169573a8071fbd0551e92ef78f4b23e3aeb5
karvind08 <karvind08@gmail.com>
2026-02-05 22:16:22 +0530

    5 February 2026

--- Unit 01/Encapsulation1.java
+++ Unit 01/Encapsulation1.java
@@ -3,15 +3,15 @@ class Student {
     int r;
     String name;
 
-    public void getdata(int i, String string) {
-        // // TODO Auto-generated method stub
-        // throw new UnsupportedOperationException("Unimplemented method 'getdata'");
-    }
+    // public void getdata(int i, String string) {
+    // // // TODO Auto-generated method stub
+    // // throw new UnsupportedOperationException("Unimplemented method 'getdata'");
+    // }
 
-    public void display() {
-        // TODO Auto-generated method stub
-        // throw new UnsupportedOperationException("Unimplemented method 'display'");
-    }
+    // public void display() {
+    // // TODO Auto-generated method stub
+    // // throw new UnsupportedOperationException("Unimplemented method 'display'");
+    // }
 }
 
 public class Encapsulation1 {
--- Unit 01/Main3.java
+++ Unit 01/Main3.java
@@ -10,7 +10,7 @@ public class Main3 {
         MyClass.greet("Arvind");
 
         // Calling via instance (possible, but not recommended)
-        MyClass obj = new MyClass();
-        obj.greet("Arvind");
+        // MyClass obj = new MyClass();
+        // obj.greet("Arvind");
     }
 }
