ab1880794bf59ea3c3b8f017ee2b426b880fadb9
karvind08 <karvind08@gmail.com>
2026-02-05 20:43:21 +0530

    5 February 2026

--- Unit 01/Student.java
+++ Unit 01/Student.java
@@ -5,6 +5,14 @@ public class Student {
     static int count = 0;
 
     void getdata(int r, String name) {
-        
+        this.r = r;
+        this.name = name;
+    }
+
+    void display() {
+        System.out.println("Roll No:" + r);
+        System.out.println("Name: " + name);
+        System.out.println("College" + College);
+        System.out.println("Object no: " + count);
     }
 }
