cd0d6974edba2c1bdddab6f275cac3d32a35358d
karvind08 <karvind08@gmail.com>
2026-02-07 13:24:23 +0530

    7 February 2026

---
+++ Unit 01/Main5.java
@@ -0,0 +1,30 @@
+class Addition {
+    int a, b;
+
+    Addition() {
+        System.out.println("Default Constructor");
+    }
+
+    Addition(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    int displaysum(Addition A, Addition B) {
+        this.a = A.a + B.a;
+        this.b = A.b + B.b;
+        int s = a + b;
+        return (s);
+    }
+}
+
+public class Main5 {
+    public static void main(String[] args) {
+        Addition obj1 = new Addition();
+        Addition A1 = new Addition(10, 20);
+        Addition A2 = new Addition(100, 200);
+        int res = obj1.displaysum(A1, A2);
+        System.out.println("The result is: " + res);
+
+    }
+}
