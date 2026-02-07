ae1169229543e3f900168669287e3d02272f4453
karvind08 <karvind08@gmail.com>
2026-02-07 14:38:35 +0530

    7 February 2026

---
+++ Unit 01/Inheritance/Main0.java
@@ -0,0 +1,19 @@
+class Parent {
+    void display() {
+        System.out.println("I am in Parent class");
+    }
+}
+
+class Child extends Parent {
+    void show() {
+        System.out.println("I am in Child Class");
+    }
+}
+
+public class Main0 {
+    public static void main(String[] args) {
+        Child C1 = new Child();
+        C1.show();
+        C1.display();
+    }
+}
