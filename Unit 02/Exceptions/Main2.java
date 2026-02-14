ce58511486b0e177032a17c4a093e56f24e7ae3f
karvind08 <karvind08@gmail.com>
2026-02-14 21:55:06 +0530

    14 February 2026

--- Unit 02/Exceptions/Main2.java
+++ Unit 02/Exceptions/Main2.java
@@ -2,7 +2,7 @@ public class Main2 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         int a = 6;
-        int b = 3;
+        int b = 0;
         try {
             int r = a / b;
             System.out.println("The result is: " + r);
---
+++ Unit 02/Exceptions/Main3.java
@@ -0,0 +1,10 @@
+public class Main3 {
+    public static void main(String[] args) {
+        try {
+            int[] a = { 1, 2, 3, 4, 5 };
+            System.out.println(a[7]);
+        } catch (Exception e) {
+            System.out.println("Something went wrong");
+        }
+    }
+}
