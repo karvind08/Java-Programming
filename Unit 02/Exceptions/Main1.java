ce85a64f4623a79590f2802287ad8c6c0c8e4562
karvind08 <karvind08@gmail.com>
2026-02-14 21:26:50 +0530

    14 February 2026

---
+++ Unit 02/Exceptions/Main1.java
@@ -0,0 +1,8 @@
+public class Main1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int a = 6;
+        int b = 0;
+        System.out.println(a / b);
+    }
+}
