9a2ee154aebff5117fb3e65f4dea237b90a13c23
karvind08 <karvind08@gmail.com>
2026-02-14 21:43:08 +0530

    14 February 2026

---
+++ Unit 02/Exceptions/Main2.java
@@ -0,0 +1,15 @@
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int a = 6;
+        int b = 3;
+        try {
+            int r = a / b;
+            System.out.println("The result is: " + r);
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+        }
+        System.out.println("End of program");
+
+    }
+}
