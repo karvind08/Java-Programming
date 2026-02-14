cec4a26de8e0b5f8590500699729744c24ecfb27
karvind08 <karvind08@gmail.com>
2026-02-14 22:04:07 +0530

    14 February 2026

---
+++ Unit 02/Exceptions/Multiple2.java
@@ -0,0 +1,15 @@
+public class Multiple2 {
+    public static void main(String[] args) {
+        try {
+            int a[] = new int[5];
+            System.out.println(a[6]);
+        } catch (ArithmeticException e) {
+            System.out.println("Arithmetic Exception occurs");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ArrayIndexOutOfBounds Exception occurs");
+        } catch (Exception e) {
+            System.out.println("Parent Exception occurs");
+        }
+        System.out.println("End of the code");
+    }
+}
