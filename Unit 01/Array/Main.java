d2f66c2bf6f8b211f9977217db1434a18aa29c16
karvind08 <karvind08@gmail.com>
2026-02-15 07:43:54 +0530

    15 February 2026

---
+++ Unit 01/Array/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String name[] = { "Java", "Python", "C", "C++", "PHP" };
+        int len = name.length;
+        for (int i = 0; i < len; i++) {
+            System.out.print(i);
+        }
+    }
+}
