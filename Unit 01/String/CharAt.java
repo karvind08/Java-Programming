bdc1baf3826d7861ff494f71bb7a6e244a0f3a8b
karvind08 <karvind08@gmail.com>
2026-02-16 09:26:00 +0530

    16 February 2026

---
+++ Unit 01/String/CharAt.java
@@ -0,0 +1,7 @@
+public class CharAt {
+    public static void main(String[] args) {
+        String S = "Java Programming";
+        char ch = S.charAt(6);
+        System.out.println(ch);
+    }
+}
