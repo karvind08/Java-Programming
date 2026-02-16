1c5fa0e4e4caca82e217e70b6e1a613a046f26ec
karvind08 <karvind08@gmail.com>
2026-02-16 18:41:26 +0530

    16 February 2026

--- Unit 01/Interface/Diamond2.java
+++ Unit 01/Interface/Diamond2.java
@@ -5,26 +5,35 @@ interface A {
 }
 
 interface B extends A {
+    @Override
     default void show() {
         System.out.println("Show from B");
     }
 }
 
 interface C extends A {
+    @Override
     default void show() {
         System.out.println("Show from C");
     }
 }
 
-class D implements B, C {
+// New interface that extends A but does NOT override show()
+interface PureA extends A {
+}
+
+class D implements B, C, PureA {
+    @Override
     public void show() {
-        C.super.show();
-        B.super.show();
+        // You can now call A's original implementation via PureA
+        // PureA.super.show(); // prints "Show from A"
+        B.super.show(); // prints "Show from B"
+        C.super.show(); // prints "Show from C"
         System.out.println("Show in D");
     }
 }
 
-public class Diamond {
+public class Diamond2 {
     public static void main(String[] args) {
         D obj = new D();
         obj.show();
