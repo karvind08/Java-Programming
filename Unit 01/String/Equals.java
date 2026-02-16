7e6f4d77594ab1c4be43fb6c627a04391bbe1d0a
karvind08 <karvind08@gmail.com>
2026-02-16 10:00:10 +0530

    16 February 2026

---
+++ Unit 01/String/Equals.java
@@ -0,0 +1,11 @@
+public class Equals {
+    public static void main(String[] args) {
+        String str = "Java";
+        String str1 = "Programming";
+        String str2 = "Java";
+        String str3 = "Object";
+        System.out.println(str.equals(str1));
+        System.out.println(str.equals(str2));
+        System.out.println(str.equals(str3));
+    }
+}
