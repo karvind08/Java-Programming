f9d6bb1bda25aa7c9c028ff4dc4f09719c0f9155
karvind08 <karvind08@gmail.com>
2026-02-16 09:43:47 +0530

    16 February 2026

---
+++ Unit 01/String/Substring.java
@@ -0,0 +1,9 @@
+public class Substring {
+    public static void main(String[] args) {
+        String str = "Java Programming";
+        String str1 = str.substring(0, 6);
+        String str2 = str.substring(3);
+        System.out.println(str1);
+        System.out.println(str2);
+    }
+}
