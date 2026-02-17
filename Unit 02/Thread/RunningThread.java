9a2ca187cf0bcc8bc0808a38a0983a53efafbb66
karvind08 <karvind08@gmail.com>
2026-02-17 16:07:15 +0530

    17 February 2026

---
+++ Unit 02/Thread/RunningThread.java
@@ -0,0 +1,30 @@
+class ChildClass1 extends Thread {
+    public void run() {
+        System.out.println("Run method of the first class.");
+    }
+}
+
+class ChildClass2 extends Thread {
+    public void run() {
+        System.out.println("Run method of the second class.");
+    }
+}
+
+class ChildClass3 extends Thread {
+    public void run() {
+        System.out.println("Run method of the third class.");
+    }
+}
+
+public class RunningThread {
+    public static void main(String args[]) {
+        ChildClass1 CC1 = new ChildClass1();
+        ChildClass2 CC2 = new ChildClass2();
+        ChildClass3 CC3 = new ChildClass3();
+
+        // starting the new thread execution.
+        CC3.start();
+        CC2.start();
+        CC1.start();
+    }
+}
