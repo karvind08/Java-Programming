ba1f9370a3f7dc2cd18bfa52fba4e0d577eb64a8
karvind08 <karvind08@gmail.com>
2026-02-17 17:16:46 +0530

    17 February 2026

---
+++ Unit 02/File/Main.java
@@ -0,0 +1,8 @@
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        File F = new File("myfile.txt");
+        System.out.println("File Created");
+    }
+}
