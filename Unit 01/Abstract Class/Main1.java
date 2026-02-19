9cb1f678fa1dfe94d6061182115f2b145da4251f
karvind08 <karvind08@gmail.com>
2026-02-19 17:52:34 +0530

    19 February 2026

--- Unit 01/Abstract Class/Main1.java
+++ Unit 01/Abstract Class/Main1.java
@@ -1,16 +1,16 @@
 abstract class Sample {
-    static void display() {
-        System.out.println("Normal Method");
-    }
-
-    final void method() {
-        System.out.println("Final method");
+    Sample() {
+        System.out.println("Hello");
     }
 
     abstract void show();
 }
 
 class A extends Sample {
+    A() {
+        System.out.println("Class A Constructor");
+    }
+
     void show() {
         System.out.println("Show in class A");
     }
@@ -20,8 +20,6 @@ public class Main1 {
     public static void main(String[] args) {
         A A1 = new A();
         A1.show();
-        Sample.display();
-        A1.method();
     }
 
 }
