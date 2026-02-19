bc51162bb09bf58d9d7509d5ce6a899b8d7e400c
karvind08 <karvind08@gmail.com>
2026-02-19 21:34:36 +0530

    19 February 2026

--- Unit 01/Package/pack1/Main1.java
+++ Unit 01/Package/pack1/Main1.java
@@ -1,18 +1,18 @@
 package pack1;
 
 class Example1 {
-    void display() {
+    public void display() {
         System.out.println("Display in Example1 and Package 1");
     }
 }
 
 class Example2 extends Example1 {
-    void show() {
+    public void show() {
         System.out.println("Show in Example2 and Package 1");
     }
 }
 
-public class Main1 {
+public class Main1 extends Example2 {
     public static void main(String[] args) {
         Example2 E = new Example2();
         E.display();
---
+++ Unit 01/Package/pack2/Hello.java
@@ -0,0 +1,11 @@
+package pack2;
+
+import pack1.Main1;
+
+public class Hello {
+    public static void main(String[] args) {
+        Main1 E = new Main1();
+        E.display();
+        E.show();
+    }
+}
