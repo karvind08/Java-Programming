569e25bc1b4c318acab300396a596b414548ba3f
karvind08 <karvind08@gmail.com>
2026-02-21 12:46:05 +0530

    21 February 2026

---
+++ Unit 02/Exceptions/Main6.java
@@ -0,0 +1,10 @@
+public class Main6 {
+    static void method() {
+        int a = 0;
+        int b = 10 / a;
+    }
+
+    public static void main(String[] args) {
+        Main6.method();
+    }
+}
