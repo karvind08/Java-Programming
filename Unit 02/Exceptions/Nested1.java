c3e8daa27990a53763006e0e163254c38298723a
karvind08 <karvind08@gmail.com>
2026-02-21 16:37:53 +0530

    21 February 2026

--- Unit 02/Exceptions/Nested1.java
+++ Unit 02/Exceptions/Nested1.java
@@ -1,3 +1,26 @@
 public class Nested1 {
+    static void nesttry(int a) {
+        try {
+            if (a == 1)
+                a = a / (a - a);
+            if (a == 2) {
+                int c[] = { 1 };
+                c[2] = 10;
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
+    public static void main(String[] args) {
+        try {
+            int a = args.length;
+            System.out.println("length:" + a);
+            int b = 10 / a;
+            System.out.println("b: " + b);
+            nesttry(a);
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
