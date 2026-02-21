bea55f7b5bf3a4cffdc6b70581b3f3a995542b8e
karvind08 <karvind08@gmail.com>
2026-02-21 13:04:13 +0530

    21 February 2026

--- Unit 02/Exceptions/Main7.java
+++ Unit 02/Exceptions/Main7.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class Main7 {
     public static void main(String[] args) {
-        int a = 15;
-        int b = 0;
+        Scanner S = new Scanner(System.in);
+        System.out.println("Enter The first Number");
+        int a = S.nextInt();
+        System.out.println("Enter The Second Number");
+        int b = S.nextInt();
         try {
             int res = a / b;
             System.out.println("The result is: " + res);
@@ -9,5 +14,6 @@ public class Main7 {
             System.out.println("Divison by zero not possible");
         }
         System.out.println("End of Program");
+        S.close();
     }
 }
