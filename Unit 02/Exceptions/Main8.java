052108f2a8b2f6e02df437bccfb0bb6a0bd457a1
karvind08 <karvind08@gmail.com>
2026-02-21 13:01:17 +0530

    21 February 2026

---
+++ Unit 02/Exceptions/Main8.java
@@ -0,0 +1,15 @@
+public class Main8 {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 0;
+        try {
+            int res = a / b;
+            System.out.println("The result is: " + res);
+        } catch (ArithmeticException e) {
+            System.out.println("Divison by zero not possible");
+        } finally {
+            System.out.println("End of Program");
+        }
+
+    }
+}
