530f683a32d58a9b9c17f37d61cf387774ead390
karvind08 <karvind08@gmail.com>
2026-02-22 10:35:44 +0530

    22 February 2026

---
+++ Unit 02/Exceptions/PropgationExample.java
@@ -0,0 +1,22 @@
+// Propagation of Exceptions
+
+class PropagationExample {
+
+    static void riskyDivision() throws ArithmeticException {
+        int result = 10 / 0; // Will throw ArithmeticException
+        System.out.println("Result: " + result);
+    }
+
+    // Method calls riskyDivision and also declares throws
+    static void callDivision() throws ArithmeticException {
+        riskyDivision();
+    }
+
+    public static void main(String[] args) {
+        try {
+            callDivision(); // Exception propagates up to here
+        } catch (ArithmeticException e) {
+            System.out.println("Handled in main: " + e.getMessage());
+        }
+    }
+}
