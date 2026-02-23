00ff71320f65d71b639043042af4cbe1c8f04d6c
karvind08 <karvind08@gmail.com>
2026-02-23 21:18:39 +0530

    23 February 2026

--- Unit 01/Package/pack1/Program1.java
+++ Unit 01/Package/pack1/Program1.java
@@ -3,13 +3,16 @@ package pack1;
 class Sample1 {
     public static int r = 10;
     public static String name = "Arvind";
+
+    public static void display() {
+        System.out.println("Static fields and methods");
+    }
 }
 
 public class Program1 extends Sample1 {
-    static int age = 30;
-
     public static void main(String[] args) {
         System.out.println(Sample1.r);
         System.out.println(Sample1.name);
+        Sample1.display();
     }
 }
--- Unit 01/Package/pack2/Program2.java
+++ Unit 01/Package/pack2/Program2.java
@@ -1,7 +1,6 @@
 package pack2;
 
 import pack1.Program1;
-import static pack1.Program1.*;
 
 public class Program2 {
     public static void main(String[] args) {
