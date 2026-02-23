18f10931f95940cf7a2fe1a9a165b027dbe9e416
karvind08 <karvind08@gmail.com>
2026-02-23 21:04:06 +0530

    23 February 2026

---
+++ Unit 01/Package/pack1/Program1.java
@@ -0,0 +1,13 @@
+package pack1;
+
+class Sample1 {
+    static int r = 10;
+    static String name = "Arvind";
+}
+
+public class Program1 {
+    public static void main(String[] args) {
+        System.out.println(Sample1.r);
+        System.out.println(Sample1.name);
+    }
+}
