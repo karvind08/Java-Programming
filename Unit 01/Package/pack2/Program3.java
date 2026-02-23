c246b0468db610851de7618ad79e61fa0adc4982
karvind08 <karvind08@gmail.com>
2026-02-23 21:26:31 +0530

    23 February 2026

---
+++ Unit 01/Package/pack2/Program3.java
@@ -0,0 +1,12 @@
+package pack2;
+
+// import pack1.Program1.*;
+// import pack1.Program1.name;
+
+public class Program3 {
+    public static void main(String[] args) {
+        // System.out.println(r);
+        // System.out.println(name);
+        // display();
+    }
+}
