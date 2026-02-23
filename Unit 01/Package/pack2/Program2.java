bf6d6e820f2e3762e830117244519cef44d9b1fc
karvind08 <karvind08@gmail.com>
2026-02-23 21:21:24 +0530

    23 February 2026

--- Unit 01/Package/pack2/Program2.java
+++ Unit 01/Package/pack2/Program2.java
@@ -4,6 +4,8 @@ import pack1.Program1;
 
 public class Program2 {
     public static void main(String[] args) {
-        System.out.println(Program1.age);
+        System.out.println(Program1.r);
+        System.out.println(Program1.name);
+        Program1.display();
     }
 }
