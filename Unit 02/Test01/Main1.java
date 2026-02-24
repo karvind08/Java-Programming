c298deb80d9992ccf123b0856b7e74eece53a50c
karvind08 <karvind08@gmail.com>
2026-02-24 21:58:34 +0530

    24 February 2026

--- Unit 02/Test01/Main1.java
+++ Unit 02/Test01/Main1.java
@@ -5,7 +5,7 @@ public class Main1 {
             int b = 10 / a;
             System.out.println("b: " + b);
             int arr[] = { 1, 2 };
-            arr[1] = 200;
+            arr[2] = 200;
             System.out.println(arr[0] + " " + arr[1]);
         } catch (ArithmeticException e) {
             System.out.println(e.getMessage());
