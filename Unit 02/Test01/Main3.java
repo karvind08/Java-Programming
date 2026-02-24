d8997046c88837b3dffcdc63db1949e70ded7983
karvind08 <karvind08@gmail.com>
2026-02-24 22:10:21 +0530

    24 February 2026

---
+++ Unit 02/Test01/Main3.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main3 {
+    static void display(int a, int b) {
+        try {
+            if (b == 0)
+                throw new ArithmeticException("Can't divide by zero");
+            else {
+                int res = a / b;
+                System.out.println(res);
+            }
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("End of display method");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner S = new Scanner(System.in);
+        System.out.println("Enter the two numbers: ");
+        int a = S.nextInt();
+        int b = S.nextInt();
+        display(a, b);
+        S.close();
+    }
+}
