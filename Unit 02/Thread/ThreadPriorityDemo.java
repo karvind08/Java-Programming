f01c393f5d7ec379e56fe4563b4365f64b2129a9
karvind08 <karvind08@gmail.com>
2026-02-25 22:37:49 +0530

    25 February 2026

---
+++ Unit 02/Thread/ThreadPriorityDemo.java
@@ -0,0 +1,30 @@
+class PriorityThread extends Thread {
+    private String threadName;
+
+    PriorityThread(String name) {
+        this.threadName = name;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(threadName + " is running with priority: " + getPriority());
+    }
+}
+
+public class ThreadPriorityDemo {
+    public static void main(String[] args) {
+        PriorityThread t1 = new PriorityThread("Thread-1");
+        PriorityThread t2 = new PriorityThread("Thread-2");
+        PriorityThread t3 = new PriorityThread("Thread-3");
+
+        // Set custom priorities
+        t1.setPriority(Thread.MIN_PRIORITY); // 1
+        t2.setPriority(Thread.NORM_PRIORITY); // 5
+        t3.setPriority(Thread.MAX_PRIORITY); // 10
+
+        // Start threads
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
