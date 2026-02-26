77cbb3d5ecb7b27797b5c85b2fcf92e7b79feda3
karvind08 <karvind08@gmail.com>
2026-02-26 22:31:11 +0530

    25 February 2026

---
+++ Unit 02/Thread/Main1.java
@@ -0,0 +1,17 @@
+class Example2 implements Runnable {
+    public void run() {
+        System.out.println("Implementing runnable");
+    }
+}
+
+public class Main1 {
+    public static void main(String[] args) {
+        Example2 E1 = new Example2();
+        Example2 E2 = new Example2();
+        Thread T1 = new Thread(E1);
+        Thread T2 = new Thread(E2);
+        T1.start();
+        T2.start();
+
+    }
+}
