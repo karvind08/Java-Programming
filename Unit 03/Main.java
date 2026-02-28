7c69a0810d24d608652cd8eb5daa6a3277171143
karvind08 <karvind08@gmail.com>
2026-02-28 22:43:32 +0530

    28 February 2026

--- Unit 03/Main.java
+++ Unit 03/Main.java
@@ -9,8 +9,9 @@ public class Main {
 
         // implementing the abstract method of the interface
         Sample solution = (int val) -> val + 51;
-
+        int r = solution.calculate(10);
         // calling the method
-        System.out.println("Ans = " + solution.calculate(51));
+        // System.out.println("Ans = " + solution.calculate(51));
+        System.out.println(r);
     }
 }
