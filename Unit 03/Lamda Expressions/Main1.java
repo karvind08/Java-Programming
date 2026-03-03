5985bd094b62d6fb88ba1041d3490925cc6aaa5a
karvind08 <karvind08@gmail.com>
2026-03-03 14:49:02 +0530

    3 March 2026

---
+++ Unit 03/Lamda Expressions/Main1.java
@@ -0,0 +1,12 @@
+interface Example {
+    void display(String m);
+}
+
+public class Main1 {
+    public static void main(String[] args) {
+        Example E = (m) -> {
+            System.out.println("Hello " + m);
+        };
+        E.display("Arvind");
+    }
+}
