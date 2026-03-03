87e05e55f693ca01abd8b814374cc3e7b475c905
karvind08 <karvind08@gmail.com>
2026-03-03 17:09:56 +0530

    3 March 2026

---
+++ Unit 03/Lamda Expressions/Main12.java
@@ -0,0 +1,21 @@
+interface MyInter4 {
+    void print();
+}
+
+class Myclass1 {
+    static int b = 500; // static variable
+
+    void show() {
+        MyInter4 t = () -> {
+            System.out.println("b =  " + b);
+        };
+        t.print();
+    }
+}
+
+public class Main12 {
+    public static void main(String arg[]) {
+        Myclass1 test = new Myclass1();
+        test.show();
+    }
+}
