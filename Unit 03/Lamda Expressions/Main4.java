33e95306f16a51f6cd6b65c5b73c223b64f17d4a
karvind08 <karvind08@gmail.com>
2026-03-03 15:14:36 +0530

    3 March 2026

--- Unit 03/Lamda Expressions/Main4.java
+++ Unit 03/Lamda Expressions/Main4.java
@@ -1,10 +1,10 @@
-interface MyName {
+interface MyName1 {
     public void SayMyName();
 }
 
 public class Main4 {
     public static void main(String[] args) {
-        MyName M = () -> {
+        MyName1 M = () -> {
             System.out.println("Arvind");
         };
         M.SayMyName();
