9cba70e8ff6a3e42d099d36a8f9c4e9112cec83d
karvind08 <karvind08@gmail.com>
2026-03-03 15:48:33 +0530

    3 March 2026

---
+++ Unit 03/Lamda Expressions/Main10.java
@@ -0,0 +1,11 @@
+interface MyComparator {
+    public boolean compare(int a1, int a2);
+}
+
+public class Main10 {
+    public static void main(String[] args) {
+        MyComparator M = (a1, a2) -> a1 > a2;
+        boolean result = M.compare(2, 5);
+        System.out.println(result);
+    }
+}
