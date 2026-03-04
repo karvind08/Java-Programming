141f32cb636993c9adbe6af19867b090e1795b65
karvind08 <karvind08@gmail.com>
2026-03-04 10:52:09 +0530

    4 March 2026

--- Unit 03/4. Base64 Encode and Decode/Encoder.java
+++ Unit 03/4. Base64 Encode and Decode/Encoder.java
@@ -3,11 +3,8 @@ import java.util.Base64;
 public class Encoder {
     public static void main(String args[]) {
         String str = "Arvind Kharwal";
-        String encodedString = Base64.getEncoder().withoutPadding().encodeToString(str.getBytes());
-        System.out.println("Encoded String(Without padding) of " + str + " is " + encodedString);
-
-        encodedString = Base64.getEncoder().encodeToString(str.getBytes());
-        System.out.println("\nEncoded String of " + str + " is " + encodedString);
+        String encodedString = Base64.getEncoder().encodeToString(str.getBytes());
+        System.out.println("Encoded String of " + str + " is " + encodedString);
     }
 
 }
