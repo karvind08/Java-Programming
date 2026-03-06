0511ef5f07d2f99cf4190880a209eb55f41417f6
karvind08 <karvind08@gmail.com>
2026-03-06 18:55:45 +0530

    6 March 2026

--- Unit 03/7. Type Inference/Main1.java
+++ Unit 03/7. Type Inference/Main1.java
@@ -1,10 +1,10 @@
 public class Main1 {
-    var x = 10;
+    // var x = 10;
     int y = 10;
 
     public static void main(String[] args) {
         Main1 M = new Main1();
         System.out.println(M.y);
-        System.out.println(M.x);
+        // System.out.println(M.x);
     }
 }
