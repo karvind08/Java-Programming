82d4b361a3926061b18a766d8f68300d79616aab
karvind08 <karvind08@gmail.com>
2026-03-06 16:01:19 +0530

    6 March 2026

---
+++ Unit 03/6. Yield/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner S = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int ch = S.nextInt();
+        switch (ch) {
+            case 1:
+                System.out.println("Case 1:");
+                yield(1);
+        }
+
+    };
+
+}
