f9ec5b8785e4a0903b1b16323449840e7a35035e
karvind08 <karvind08@gmail.com>
2026-03-06 18:43:18 +0530

    6 March 2026

---
+++ Unit 03/7. Type Inference/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        var a = 10;
+        var b = 12.5;
+        var s = "Hello";
+        System.out.println(a + "\n" + b + "\n" + s);
+    }
+}
