b316c4a6cd70ff2e7a15913e930973c77142989e
karvind08 <karvind08@gmail.com>
2026-03-06 15:51:11 +0530

    6 March 2026

--- Unit 03/5. Sealed Classes/Main.java
+++ Unit 03/5. Sealed Classes/Main.java
@@ -1,3 +1,21 @@
-public class Main {
+class A {
+    // members of class A
+}
 
+class B extends A {
+    // members of class B
+}
+
+final class C extends A {
+    // members of class C
+}
+
+class D {
+    // independent class, not related to A
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // test code here
+    }
 }
