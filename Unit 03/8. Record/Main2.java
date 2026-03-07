02fa83218dc4f1ca304b388ce883e17c12cb11b9
karvind08 <karvind08@gmail.com>
2026-03-07 11:35:55 +0530

    7 March 2026

--- Unit 03/8. Record/Main2.java
+++ Unit 03/8. Record/Main2.java
@@ -20,14 +20,14 @@ class Example {
         return "Example [r=" + r + ", name=" + name + "]";
     }
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + r;
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        return result;
-    }
+    // @Override
+    // public int hashCode() {
+    // final int prime = 31;
+    // int result = 1;
+    // result = prime * result + r;
+    // result = prime * result + ((name == null) ? 0 : name.hashCode());
+    // return result;
+    // }
 
     @Override
     public boolean equals(Object obj) {
