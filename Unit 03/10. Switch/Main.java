9db8a6ed778f75fa9f00d046e7772e37cfc946dd
karvind08 <karvind08@gmail.com>
2026-03-07 13:52:13 +0530

    7 March 2026

--- Unit 03/10. Switch/Main.java
+++ Unit 03/10. Switch/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner S = new Scanner(System.in);
+        System.out.println("Enter your choice");
+        int ch = S.nextInt();
+        switch (ch) {
+            case 1:
+                System.out.println("Case 1");
+            case 2:
+                System.out.println("Case 2");
+            default:
+                System.out.println("Default");
+        }
+        S.close();
     }
 }
