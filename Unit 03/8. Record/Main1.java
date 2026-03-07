8a399b662da740f77c77b383bba149f93d417028
karvind08 <karvind08@gmail.com>
2026-03-07 11:29:03 +0530

    7 March 2026

--- Unit 03/8. Record/Main1.java
+++ Unit 03/8. Record/Main1.java
@@ -23,12 +23,9 @@ class Example {
 
 }
 
-public class Main {
+public class Main1 {
     public static void main(String[] args) {
         Example E = new Example(1, "Arvind");
-        System.out.println(E.getR());
-        System.out.println(E.getName());
-        System.out.println(E);
 
     }
 
