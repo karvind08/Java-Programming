45ad332a16b02354f90c6be0460c9f7df7b25b8c
karvind08 <karvind08@gmail.com>
2026-03-07 23:50:09 +0530

    7 March 2026

---
+++ Unit 04/ArrayList/ArrayList2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class ArrayList2 {
+    public static void main(String args[]) {
+        // create an ArrayList of String type and Initialize an ArrayList with asList()
+        ArrayList<String> A1 = new ArrayList<String>(
+                Arrays.asList("I",
+                        "love",
+                        "Java"));
+
+        // to print the ArrayList
+        System.out.println("ArrayList is " + A1);
+    }
+}
