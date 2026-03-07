b1d1669a924bd2ff9c0bfa4eee320a2ecfb41270
karvind08 <karvind08@gmail.com>
2026-03-07 23:42:22 +0530

    7 March 2026

---
+++ Unit 04/ArrayList/ArrayList1.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class ArrayList1 {
+    public static void main(String args[]) {
+        // create an ArrayList of String type
+        ArrayList<String> str = new ArrayList<String>();
+
+        // Initialize an ArrayList with add()
+        str.add("Hello");
+        str.add("World");
+        str.add("Java");
+
+        // to print the ArrayList
+        System.out.println("ArrayList is" + str);
+    }
+}
