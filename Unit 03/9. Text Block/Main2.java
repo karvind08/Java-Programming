cf442f9e3d44030ab0d677ee327d4919e3d172a0
karvind08 <karvind08@gmail.com>
2026-03-07 12:52:44 +0530

    7 March 2026

---
+++ Unit 03/9. Text Block/Main2.java
@@ -0,0 +1,6 @@
+public class Main2 {
+    public static void main(String[] args) {
+        String str = "Hello %s, I mam %d years old".formatted("Arvind", 44);
+        System.out.println(str);
+    }
+}
