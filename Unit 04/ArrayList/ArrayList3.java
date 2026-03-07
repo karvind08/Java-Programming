e12b0b29cc6b8ed1ae75a8238eb2a0f9bf5d3dba
karvind08 <karvind08@gmail.com>
2026-03-07 23:54:46 +0530

    7 March 2026

---
+++ Unit 04/ArrayList/ArrayList3.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class ArrayList3 {
+    public static void main(String args[]) {
+        List<String> str = new ArrayList<>(List.of("Coding", "is", "Fun"));
+        System.out.println("ArrayList is " + str);
+    }
+}
