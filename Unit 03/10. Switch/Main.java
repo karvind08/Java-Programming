d0d8e7033e475b3ccd0ae0e444e9417115f5876d
karvind08 <karvind08@gmail.com>
2026-03-07 13:53:12 +0530

    7 March 2026

--- Unit 03/10. Switch/Main.java
+++ Unit 03/10. Switch/Main.java
@@ -8,8 +8,10 @@ public class Main {
         switch (ch) {
             case 1:
                 System.out.println("Case 1");
+                break;
             case 2:
                 System.out.println("Case 2");
+                break;
             default:
                 System.out.println("Default");
         }
