36efad9ca59df919184991ad2e04218294cd502a
karvind08 <karvind08@gmail.com>
2026-03-07 12:49:32 +0530

    7 March 2026

---
+++ Unit 03/9. Text Block/Main1.java
@@ -0,0 +1,16 @@
+public class Main1 {
+    public static void main(String[] args) {
+        String str = """
+                <html lang="en">
+                <head>
+                <title>Document</title>
+                </head>
+                    body>
+                        <h1>Hello</h1>
+                    </body>
+                </html>
+                """;
+        ;
+        System.out.println(str);
+    }
+}
