51203e63d7e5965ce04176332d864309c6266432
karvind08 <karvind08@gmail.com>
2026-03-07 12:30:17 +0530

    7 March 2026

---
+++ Unit 03/8. Record/Record4.java
@@ -0,0 +1,9 @@
+record Person(int r, String name) {
+}
+
+public class Record4 {
+    public static void main(String[] args) {
+        Person P1 = new Person(1, "Arvind");
+        System.out.println(P1.name());
+    }
+}
