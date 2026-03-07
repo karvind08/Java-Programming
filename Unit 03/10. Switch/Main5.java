05645a9ee97161f5117de0458c7cea1aad657a71
karvind08 <karvind08@gmail.com>
2026-03-07 14:19:36 +0530

    7 March 2026

--- Unit 03/10. Switch/Main5.java
+++ Unit 03/10. Switch/Main5.java
@@ -5,14 +5,12 @@ public class Main5 {
         Scanner S = new Scanner(System.in);
         System.out.println("Enter your choice");
         int ch = S.nextInt();
-        int r = 0;
         int res = switch (ch) {
-            case 1 -> r = 10;
-            case 2 -> r = 20;
-            default -> r = 100;
+            case 1 -> 10;
+            case 2 -> 20;
+            default -> 100;
         };
         System.out.println(res);
-        System.out.println(r);
         S.close();
     }
 }
