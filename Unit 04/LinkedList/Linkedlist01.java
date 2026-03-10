02c4026b9d357edd2357b69e978eeaac65d752d9
karvind08 <karvind08@gmail.com>
2026-03-10 21:02:28 +0530

    10 March 2026

--- Unit 04/LinkedList/Linkedlist01.java
+++ Unit 04/LinkedList/Linkedlist01.java
@@ -1,3 +1,12 @@
-public class Linkedlist01 {
-    
+import java.util.*;
+
+class Linkedlist01 {
+    public static void main(String args[]) {
+        LinkedList<Integer> LL = new LinkedList<Integer>();
+        LL.add(1);
+        LL.add(3);
+        LL.add(4);
+        LL.add(1, 2);
+        System.out.println(LL);
+    }
 }
