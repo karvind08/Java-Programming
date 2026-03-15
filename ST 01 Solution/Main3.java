c0331fc67b29b9b026e86231875a20f0bd67b470
karvind08 <karvind08@gmail.com>
2026-03-15 11:03:56 +0530

    15 March 2026

--- ST 01 Solution/Main3.java
+++ ST 01 Solution/Main3.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class InvalidMark extends Exception {
     public InvalidMark(String message) {
         super(message);
@@ -6,6 +8,9 @@ class InvalidMark extends Exception {
 
 public class Main3 {
     void grade(int[] marks) throws InvalidMark {
+        System.out.println("The array in grade: ");
+        for (int i = 0; i < 5; i++)
+            System.out.println(marks[i]);
         int s = 0;
         for (int i : marks)
             if (i < 0 || i > 100)
@@ -29,13 +34,18 @@ public class Main3 {
     }
 
     public static void main(String[] args) {
-        // int marks[] = new int[5];
-        int[] marks = { 10, 80, 90, 100, 67 };
+        Scanner S = new Scanner(System.in);
+        int marks[] = new int[5];
+        System.out.println("Enter the marks: ");
+        for (int i = 0; i < 5; i++)
+            marks[i] = S.nextInt();
         Main3 M = new Main3();
         try {
             M.grade(marks);
         } catch (InvalidMark e) {
             System.out.println(e.getMessage());
+        } finally {
+            S.close();
         }
 
     }
