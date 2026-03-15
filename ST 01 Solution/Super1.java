fa99764e3c77f983eb9049a0c8b147598cf0e735
karvind08 <karvind08@gmail.com>
2026-03-15 22:16:47 +0530

    15 March 2026

---
+++ ST 01 Solution/Super1.java
@@ -0,0 +1,35 @@
+class Animal1 {
+    String type = "Animal";
+
+    Animal1() {
+        System.out.println("Animal Constructor called");
+    }
+
+    void sound() {
+        System.out.println("Animal makes sound");
+    }
+}
+
+class Dog1 extends Animal1 {
+    String type = "Dog";
+
+    Dog1() {
+        System.out.println("Dog Constructor called");
+    }
+
+    void sound() {
+        System.out.println("Dog Barks");
+    }
+
+    void show() {
+        System.out.println(type);
+    }
+}
+
+public class Super1 {
+    public static void main(String[] args) {
+        Dog1 D = new Dog1();
+        D.sound();
+        D.show();
+    }
+}
