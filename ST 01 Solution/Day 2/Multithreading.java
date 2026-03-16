60575f4e1edba12949b15bf6ec83ce0169f2481f
karvind08 <karvind08@gmail.com>
2026-03-16 22:01:08 +0530

    16 March 2026

---
+++ ST 01 Solution/Day 2/Multithreading.java
@@ -0,0 +1,24 @@
+class Example extends Thread {
+    public void run() {
+        for (int i = 1; i <= 5; i++)
+            System.out.print(i);
+    }
+}
+
+class Sample implements Runnable {
+    public void run() {
+        for (char i = 'A'; i < 'F'; i++)
+            System.out.print(i);
+    }
+}
+
+public class Multithreading {
+    public static void main(String[] args) {
+        Example E1 = new Example();
+        Sample S1 = new Sample();
+        Thread T1 = new Thread(S1);
+        // E1.start();
+        // T1.start();
+
+    }
+}
