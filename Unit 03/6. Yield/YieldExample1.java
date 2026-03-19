5f913ce1f08004cbc485d5d94df1769b770a1376
karvind08 <karvind08@gmail.com>
2026-03-19 22:52:19 +0530

    19 March 2026

--- Unit 03/6. Yield/YieldExample1.java
+++ Unit 03/6. Yield/YieldExample1.java
@@ -7,8 +7,12 @@ public class YieldExample1 implements Runnable {
 
     public static void main(String[] args) {
         YieldExample1 y1 = new YieldExample1();
-        Thread T1 = new Thread(y1,"First Child");
+        Thread T1 = new Thread(y1, "First Child");
         YieldExample1 y2 = new YieldExample1();
-        Thread T2 = new Thread(y2,"Second Child");
+        Thread T2 = new Thread(y2, "Second Child");
+        T1.setPriority(6);
+        T2.setPriority(9);
+        T1.start();
+        T2.start();
     }
 }
