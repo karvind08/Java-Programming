6f6a4b5bc610544225ec9f7214263df2c6b61868
karvind08 <karvind08@gmail.com>
2026-03-20 20:29:18 +0530

    20 March 2026

--- Unit 03/12. Stream API/Main1.java
+++ Unit 03/12. Stream API/Main1.java
@@ -5,10 +5,9 @@ import java.util.stream.Stream;
 public class Main1 {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 2, 3, 4);
-        System.out.println(list);
         Stream<Integer> data = list.stream();
         long count = data.count();
         System.out.println(count);
-        data.forEach(n -> System.out.print(n));
+        // data.forEach(n -> System.out.print(n));
     }
 }
