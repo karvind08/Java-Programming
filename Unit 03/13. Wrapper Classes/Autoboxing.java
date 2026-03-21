df8908393819789b7b4120e0f280f0710a6f79a1
karvind08 <karvind08@gmail.com>
2026-03-21 14:19:33 +0530

    21 March 2026

--- Unit 03/13. Wrapper Classes/Autoboxing.java
+++ Unit 03/13. Wrapper Classes/Autoboxing.java
@@ -2,6 +2,7 @@ public class Autoboxing {
     public static void main(String[] args) {
         int i = 100;
         Integer I1 = Integer.valueOf(i);
+        I1 = i;
         System.out.println(I1);
     }
 }
---
+++ Unit 03/13. Wrapper Classes/Unboxing.java
@@ -0,0 +1,7 @@
+public class Unboxing {
+    public static void main(String[] args) {
+        Integer I1 = 10;
+        System.out.println(I1);
+
+    }
+}
