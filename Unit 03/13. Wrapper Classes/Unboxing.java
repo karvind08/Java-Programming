d3091a38567c883ef5a6b1fe76387a127b5f2178
karvind08 <karvind08@gmail.com>
2026-03-21 14:22:46 +0530

    21 March 2026

--- Unit 03/13. Wrapper Classes/Unboxing.java
+++ Unit 03/13. Wrapper Classes/Unboxing.java
@@ -1,8 +1,8 @@
 public class Unboxing {
     public static void main(String[] args) {
-        Integer I1 = 10;
+        Integer I1 = 10; // Autoboxing
         System.out.println(I1);
-        int num = I1;
+        int num = I1; // Auto-unboxing
         System.out.println(num);
     }
 }
