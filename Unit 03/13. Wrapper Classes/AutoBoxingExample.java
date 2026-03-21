ef718da20fd7c2e6b4a99a8b8b23d8cf062fce57
karvind08 <karvind08@gmail.com>
2026-03-21 14:07:16 +0530

    21 March 2026

--- Unit 03/13. Wrapper Classes/AutoBoxingExample.java
+++ Unit 03/13. Wrapper Classes/AutoBoxingExample.java
@@ -1,15 +1,16 @@
-public class AutoBoxingExample{  
-    public static void main(String args[]){     
+public class AutoBoxingExample {
+    public static void main(String args[]) {
         char ch = 's';
-        //Autoboxing- primitive to Character object conversion
+        // Autoboxing- primitive to Character object conversion
         Character s = ch;
-        int a=50; 
-        //Converting int into Integer explicitly
-        Integer first=Integer.valueOf(a);
-        //Autoboxing, now compiler will write Integer.valueOf(a) internally and hence, doesn't generate an error  
-        Integer second=a;
-        System.out.println(a);  
+        int a = 50;
+        // Converting int into Integer explicitly
+        Integer first = Integer.valueOf(a);
+        // Autoboxing, now compiler will write Integer.valueOf(a) internally and hence,
+        // doesn't generate an error
+        Integer second = a;
+        System.out.println(a);
         System.out.println(first);
         System.out.println(second);
     }
-} 
+}
