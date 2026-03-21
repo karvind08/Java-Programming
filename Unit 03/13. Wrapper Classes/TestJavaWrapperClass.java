53aef9b80778393d0f03832fe3bfd6ad8e0eb7b6
karvind08 <karvind08@gmail.com>
2026-03-21 16:28:01 +0530

    21 March 2026

---
+++ Unit 03/13. Wrapper Classes/TestJavaWrapperClass.java
@@ -0,0 +1,31 @@
+class SpeedWrapperClass {
+    private int speed;
+
+    SpeedWrapperClass() {
+    }
+
+    SpeedWrapperClass(int speed) {
+        this.speed = speed;
+    }
+
+    public int getVehicleSpeed() {
+        return speed;
+    }
+
+    public void setVehicleSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    @Override
+    public String toString() {
+        return Integer.toString(speed);
+    }
+}
+
+// Testing the custom wrapper class
+public class TestJavaWrapperClass {
+    public static void main(String[] args) {
+        SpeedWrapperClass speedValue = new SpeedWrapperClass(100);
+        System.out.println(speedValue);
+    }
+}
