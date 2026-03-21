143b159a42ed25f418926f1eefd8390032d908a2
karvind08 <karvind08@gmail.com>
2026-03-21 16:08:25 +0530

    21 March 2026

--- Unit 03/13. Wrapper Classes/Autoboxing.java
+++ Unit 03/13. Wrapper Classes/Autoboxing.java
@@ -4,5 +4,10 @@ public class Autoboxing {
         Integer I1 = Integer.valueOf(i);
         I1 = i;
         System.out.println(I1);
+        System.out.println("=========");
+        Integer I2 = Integer.valueOf(20);
+        System.out.println(I2);
+        Integer I3 = 30;
+        System.out.println(I3);
     }
 }
