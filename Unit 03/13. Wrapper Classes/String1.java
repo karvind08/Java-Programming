36c1bdb74a68b0df3cf78d71b01d026244137092
karvind08 <karvind08@gmail.com>
2026-03-21 14:26:18 +0530

    21 March 2026

---
+++ Unit 03/13. Wrapper Classes/String1.java
@@ -0,0 +1,7 @@
+public class String1 {
+    public static void main(String[] args) {
+        String str = "102";
+        int n = Integer.parseInt(str);
+        System.out.println(n * 2);
+    }
+}
