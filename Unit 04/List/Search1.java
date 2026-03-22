24883be73bda729bb117a57718d31b282dda1a7f
karvind08 <karvind08@gmail.com>
2026-03-22 14:55:09 +0530

    22 March 2026

---
+++ Unit 04/List/Search1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Search1 {
+    public static void main(String[] args) {
+        List<String> words = new ArrayList<>();
+        words.add("apple");
+        words.add("banana");
+        words.add("orange");
+        words.add("banana");
+        int index = words.indexOf("banana");
+        System.out.println("The first occurrence of 'banana' is at index " + index);
+    }
+}
