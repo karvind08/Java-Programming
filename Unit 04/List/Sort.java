a116a6f7e2e490db1a7a975b1c89d4b2f82637ca
karvind08 <karvind08@gmail.com>
2026-03-22 14:32:18 +0530

    22 March 2026

--- Unit 04/List/Sort.java
+++ Unit 04/List/Sort.java
@@ -7,5 +7,7 @@ public class Sort {
         list.add(2);
         list.add(1);
         System.out.println(list);
+        Collections.sort(list);
+        System.out.println("The sorted list: "+list);
     }
 }
