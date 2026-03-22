72836e11bf5ab09ac21ff625ccdd4101ba730892
karvind08 <karvind08@gmail.com>
2026-03-22 14:30:44 +0530

    22 March 2026

---
+++ Unit 04/List/Sort.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Sort {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(3);
+        list.add(2);
+        list.add(1);
+        System.out.println(list);
+    }
+}
