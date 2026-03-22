e4c4b6bb3c775ec86f94ad52ac79dde9e672c964
karvind08 <karvind08@gmail.com>
2026-03-22 14:45:59 +0530

    22 March 2026

---
+++ Unit 04/List/RemoveElement.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class RemoveElement {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        System.out.println("The list is: " + list);
+        list.remove(3);
+        System.out.println("The Updated list is: " + list);
+        list.remove(new Integer(20)); // deprecated
+        System.out.println("The Updated list is: " + list);
+    }
+}
