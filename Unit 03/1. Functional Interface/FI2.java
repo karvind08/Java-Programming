f2021afee2c76c969052c2edb68711397a9a3562
karvind08 <karvind08@gmail.com>
2026-03-23 22:02:39 +0530

    23 March 2026

---
+++ Unit 03/1. Functional Interface/FI2.java
@@ -0,0 +1,16 @@
+interface Animal {
+    String animalname(String name);
+}
+
+public class FI2 implements Animal {
+    public String animalname(String name) {
+        return "Hello" + name;
+    }
+
+    public static void main(String[] args) {
+        FI2 F = new FI2();
+        String res = F.animalname("Lion");
+        System.out.println(res);
+
+    }
+}
