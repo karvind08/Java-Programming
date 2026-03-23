16606e054683732315cbd96099bd9356316e555c
karvind08 <karvind08@gmail.com>
2026-03-23 22:41:16 +0530

    23 March 2026

--- Unit 03/1. Functional Interface/Main5.java
+++ Unit 03/1. Functional Interface/Main5.java
@@ -4,9 +4,7 @@ interface Example {
 
 public class Main5 {
     public static void main(String[] args) {
-        Example E1 = () -> {
-            System.out.println("Display Method");
-        };
+        Example E1 = () -> System.out.println("Display Method");
         E1.display();
     }
 
