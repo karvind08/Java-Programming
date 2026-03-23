a89871f9ba9dcc1aa90cb6a98bb533c3c0213ce4
karvind08 <karvind08@gmail.com>
2026-03-23 20:39:22 +0530

    23 March 2026

--- Unit 03/6. Yield/Main1.java
+++ Unit 03/6. Yield/Main1.java
@@ -10,8 +10,8 @@ public class Main1 {
     public static void main(String[] args) {
         Class1 C1 = new Class1();
         Class1 C2 = new Class1();
-        Thread T1 = new Thread(C1);
-        Thread T2 = new Thread(C2);
+        Thread T1 = new Thread(C1, "First Thread");
+        Thread T2 = new Thread(C2, "Second Thread");
         T1.setPriority(Thread.MAX_PRIORITY);
         T2.setPriority(Thread.MIN_PRIORITY);
         T1.start();
