a658b30b0d56857c66badc1b15b161038a730480
karvind08 <karvind08@gmail.com>
2026-03-26 21:12:27 +0530

    26 March 2026

--- Unit 03/1. Functional Interface/Main0.java
+++ Unit 03/1. Functional Interface/Main0.java
@@ -18,18 +18,23 @@ interface Sample1 {
     }
 }
 
-public class Main0 {
+public class Main0 implements Sample1 {
+    public double square(double r) {
+        return r * r;
+    }
+
     public static void main(String[] args) {
-        Sample1 S1 = (double d) -> d * d;
-        double res = S1.square(2.4d);
-        System.out.println(res);
+        Main0 M1 = new Main0();
         int r1 = Sample1.div(6, 3);
         System.out.println(r1);
         int r2 = Sample1.mul(5, 3);
         System.out.println(r2);
-        int r3 = S1.add(10, 20);
+        int r3 = M1.add(10, 20);
         System.out.println(r3);
-        int r4 = S1.sub(50, 40);
+        int r4 = M1.sub(50, 40);
         System.out.println(r4);
+        double r5 = M1.square(5);
+        System.out.println(r5);
+
     }
 }
