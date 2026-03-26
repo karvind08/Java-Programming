e4fa986e58a4721f6e5434657d98e0d7e3a3e6fa
karvind08 <karvind08@gmail.com>
2026-03-26 15:28:23 +0530

    26 March 2026

--- Unit 03/11. Annotations/Main2.java
+++ Unit 03/11. Annotations/Main2.java
@@ -1,11 +1,18 @@
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 @interface Employee {
     String name();
 
-    int Salary();
+    int salary();
 }
 
+@Employee(name = "Arvind", salary = 1000)
 public class Main2 {
     public static void main(String[] args) {
-
+        Employee E1 = Main2.class.getAnnotation(Employee.class);
+        System.out.println(E1.name());
+        System.out.println(E1.salary());
     }
 }
