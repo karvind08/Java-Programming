3232c2df7e3189d0f8ef33f70f2d8e8b38aff860
karvind08 <karvind08@gmail.com>
2026-03-26 15:44:04 +0530

    26 March 2026

--- Unit 03/11. Annotations/Main3.java
+++ Unit 03/11. Annotations/Main3.java
@@ -19,7 +19,7 @@ import java.lang.annotation.RetentionPolicy;
 @Employee1(name = "Yuvaan", salary = 5000)
 public class Main3 {
     public static void main(String[] args) {
-        Employee1[] E1 = Main2.class.getAnnotationsByType(Employee1.class);
+        Employee1[] E1 = Main3.class.getAnnotationsByType(Employee1.class);
         for (Employee1 e : E1) {
             System.out.println(e.name());
             System.out.println(e.salary());
