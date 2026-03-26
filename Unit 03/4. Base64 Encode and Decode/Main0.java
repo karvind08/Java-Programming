08a16cbdcaf3752119d57102b1d24691b9d53acc
karvind08 <karvind08@gmail.com>
2026-03-26 20:53:52 +0530

    26 March 2026

--- Unit 03/4. Base64 Encode and Decode/Main0.java
+++ Unit 03/4. Base64 Encode and Decode/Main0.java
@@ -6,6 +6,9 @@ public class Main0 {
         String encodedstring = Base64.getEncoder().withoutPadding().encodeToString(str.getBytes());
         // String encodedstring = Base64.getEncoder().encodeToString(str.getBytes());
         System.out.println(encodedstring);
+        byte[] decodedBytes = Base64.getDecoder().decode(encodedstring);
+        String decodedstring = new String(decodedBytes);
+        System.out.println(decodedstring);
     }
 
 }
