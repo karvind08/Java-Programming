3f8279f54621253f7cac995b0ca4ea31bbf0f51a
karvind08 <karvind08@gmail.com>
2026-03-26 15:53:36 +0530

    26 March 2026

---
+++ Unit 03/11. Annotations/Main5.java
@@ -0,0 +1,20 @@
+class Example {
+    @Override
+    void display(String name) {
+        System.out.println("Display in Example" + name);
+    }
+}
+
+class Sample extends Example {
+    void display(String name) {
+        System.out.println("Display in Sample " + name);
+    }
+}
+
+public class Main5 {
+    public static void main(String[] args) {
+        Sample S = new Sample();
+        S.display("Hello");
+
+    }
+}
