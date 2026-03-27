dd0fd93cda32dc65f08a67c19f68979e6b3dd0f7
karvind08 <karvind08@gmail.com>
2026-03-27 19:29:16 +0530

    27 March 2026

--- Unit 03/3. Method References/Main2.java
+++ Unit 03/3. Method References/Main2.java
@@ -1,7 +1,14 @@
+@FunctionalInterface
 interface Operation {
     int add(int a, int b);
 }
 
 public class Main2 {
-
+    public static void main(String[] args) {
+        Operation O = (int a, int b) -> {
+            return a + b;
+        };
+        int r = O.add(10, 20);
+        System.out.println(r);
+    }
 }
