08b516b4df309aadc3cdd162c81f0e56dc55cb3f
karvind08 <karvind08@gmail.com>
2026-03-27 20:56:46 +0530

    27 March 2026

--- Unit 03/14. forEach Method/Main.java
+++ Unit 03/14. forEach Method/Main.java
@@ -1,6 +1,14 @@
 import java.util.*;
+import java.util.function.Consumer;
+
 public class Main {
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(1,2,3,4,5);
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        Consumer<Integer> Obj = new Consumer<Integer>() {
+            public void accept(Integer n) {
+                System.out.println(n);
+            }
+        };
+        list.forEach(Obj);
     }
 }
