406235931b53c8651a3930a0b72e4062497bb0d0
karvind08 <karvind08@gmail.com>
2026-03-27 21:06:07 +0530

    27 March 2026

--- Unit 03/14. forEach Method/Main0.java
+++ Unit 03/14. forEach Method/Main0.java
@@ -4,9 +4,7 @@ import java.util.function.Consumer;
 public class Main0 {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
-        Consumer<Integer> Obj = (Integer n) -> {
-            System.out.println(n);
-        };
+        Consumer<Integer> Obj = (Integer n) -> System.out.println(n);
         list.forEach(Obj);
     }
 }
