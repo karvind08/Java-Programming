d7aa6ecb110cf7051e535d98617f81f41dbc609d
karvind08 <karvind08@gmail.com>
2026-03-27 21:09:04 +0530

    27 March 2026

---
+++ Unit 03/14. forEach Method/Main1.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class Main1 {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        list.forEach((Integer n) -> System.out.println(n));
+    }
+}
