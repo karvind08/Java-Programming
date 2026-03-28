c97703c42b9040ed26b053a74c6ff9bb7e87e84c
karvind08 <karvind08@gmail.com>
2026-03-28 23:22:10 +0530

    28 March 2026

--- Unit 03/16. Try with Resources/Main.java
+++ Unit 03/16. Try with Resources/Main.java
@@ -2,17 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner S = new Scanner(System.in);
-        try {
+
+        try (Scanner S = new Scanner(System.in)) {
             System.out.println("Enter the number: ");
             int a = S.nextInt();
             System.out.println(a);
         } catch (Exception e) {
             System.out.println(e.getMessage());
-        } finally {
-            S.close();
         }
-
     }
 
 }
