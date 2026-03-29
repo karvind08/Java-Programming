f566638ed3ea01ff1981ea56436d96cb3ff74678
karvind08 <karvind08@gmail.com>
2026-03-29 15:48:14 +0530

    29 March 2026

--- Unit 03/15. Anonymous Inner Class/Main2.java
+++ Unit 03/15. Anonymous Inner Class/Main2.java
@@ -1,8 +1,8 @@
-import java.util.*;
+// import java.util.*;
 
 public class Main2 {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<Integer>();
-        List<Integer> list1 = new ArrayList<>();
+        // List<Integer> list = new ArrayList<Integer>();
+        // List<Integer> list1 = new ArrayList<>();
     }
 }
---
+++ Unit 03/15. Anonymous Inner Class/Main3.java
@@ -0,0 +1,14 @@
+abstract class Test<T> {
+    abstract T display(T a, T b);
+}
+
+public class Main3 {
+    public static void main(String[] args) {
+        Test<Integer> T1 = new Test<>() {
+            Integer display(Integer a, Integer b) {
+                return a + b;
+            }
+        };
+        System.out.println(T1.display(10, 20));
+    }
+}
