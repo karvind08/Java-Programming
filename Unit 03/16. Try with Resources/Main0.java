9502768a8bdef46cd5ee305553613c46b8b92f28
karvind08 <karvind08@gmail.com>
2026-03-29 15:31:37 +0530

    29 March 2026

--- Unit 03/16. Try with Resources/Main0.java
+++ Unit 03/16. Try with Resources/Main0.java
@@ -1,3 +1,20 @@
+import java.io.FileInputStream;
+import java.util.Scanner;
+
 public class Main0 {
+    public static void main(String[] args) {
+
+        try (Scanner S = new Scanner(System.in);
+                FileInputStream FIS = new FileInputStream("demo.txt")) {
+            System.out.println("Enter the number: ");
+            int a = S.nextInt();
+            System.out.println(a);
+            byte[] b = FIS.readAllBytes();
+            String str = new String(b);
+            System.out.println(str);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
 
 }
