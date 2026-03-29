8589fc2f7ed9bec0e061f0ac630357fd54eefa9c
karvind08 <karvind08@gmail.com>
2026-03-29 15:42:10 +0530

    29 March 2026

---
+++ Unit 03/15. Anonymous Inner Class/Main2.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Main2 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<Integer>();
+        List<Integer> list1 = new ArrayList<>();
+    }
+}
