9ce34d67070a649b9c9808906ceff7bd78ce8d31
karvind08 <karvind08@gmail.com>
2026-03-31 18:38:47 +0530

    31 March 2026

--- Unit 03/15. Anonymous Inner Class/Example1.java
+++ Unit 03/15. Anonymous Inner Class/Example1.java
@@ -1,3 +1,14 @@
-public class Example1 {
+abstract class Diamond<T> {
+    abstract T display(T a, T b);
+}
 
+public class Example1 {
+    public static void main(String[] args) {
+        Diamond<Integer> D1 = new Diamond<>() {
+            Integer display(Integer a, Integer b) {
+                return a + b;
+            }
+        };
+        System.out.println(D1.display(10, 20));
+    }
 }
