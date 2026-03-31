79eb9ed41e08ed8c047022e4f2596232d8cbbdd3
karvind08 <karvind08@gmail.com>
2026-03-31 11:02:27 +0530

    31 March 2026

--- Unit 03/12. Stream API/Stream.java
+++ Unit 03/12. Stream API/Stream.java
@@ -4,5 +4,6 @@ public class Stream {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(10, 2, 13, -4, 5);
         list.stream().map(n -> n * 2).forEach(n -> System.out.println(n));
+        list.forEach(n -> System.out.println(n));
     }
 }
