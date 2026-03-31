66f73508581f3e1828539c936e52cef420405d0d
karvind08 <karvind08@gmail.com>
2026-03-31 18:04:06 +0530

    31 March 2026

--- Unit 03/15. Anonymous Inner Class/Inner5.java
+++ Unit 03/15. Anonymous Inner Class/Inner5.java
@@ -14,5 +14,7 @@ public class Inner5 {
     public static void main(String[] args) {
         OuterClass OC1 = new OuterClass();
         OC1.display();
+        OuterClass.InnerClass IC1 = OC1.new InnerClass();
+        IC1.show();
     }
 }
