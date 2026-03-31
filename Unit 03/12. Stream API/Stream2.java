f8fba3ac4923413e89ef4d4e098e7671d72558ec
karvind08 <karvind08@gmail.com>
2026-03-31 11:19:39 +0530

    31 March 2026

--- Unit 03/12. Stream API/Stream2.java
+++ Unit 03/12. Stream API/Stream2.java
@@ -1,11 +1,13 @@
 import java.util.*;
-import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class Stream2 {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 7, 3, 8, 2);
         List<Integer> res = list.stream()
                 .map(n -> n * 2)
-                .collect(Collector.toList());
+                .collect(Collectors.toList());
+        System.out.println();
+        res.forEach(n -> System.out.print(n + " "));
     }
 }
