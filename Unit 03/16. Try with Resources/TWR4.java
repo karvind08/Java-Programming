ce5a6cddd945b51003cefa0134d365e10eaadd72
karvind08 <karvind08@gmail.com>
2026-03-31 17:00:16 +0530

    31 March 2026

--- Unit 03/16. Try with Resources/TWR4.java
+++ Unit 03/16. Try with Resources/TWR4.java
@@ -1,3 +1,18 @@
-public class TWR4 {
+import java.io.FileInputStream;
+import java.util.Scanner;
 
+public class TWR4 {
+    public static void main(String[] args) {
+        try (Scanner S = new Scanner(System.in);
+                FileInputStream FIS = new FileInputStream("demo1.txt")) {
+            System.out.println("Enter the number: ");
+            int n = S.nextInt();
+            System.out.println("The number is " + n);
+            byte[] b = FIS.readAllBytes();
+            String str = new String(b);
+            System.out.println(str);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
 }
