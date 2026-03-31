fb6bcede6c1fc564e380572e6c0006490815aa1c
karvind08 <karvind08@gmail.com>
2026-03-31 18:17:13 +0530

    31 March 2026

--- Unit 03/15. Anonymous Inner Class/Inner6.java
+++ Unit 03/15. Anonymous Inner Class/Inner6.java
@@ -1,9 +1,9 @@
 class OuterClass1 {
     void display() {
-        System.out.println("display in OuterClass1");
+        System.out.println("Display in OuterClass1");
     }
 
-    class InnerClass1 {
+    static class InnerClass1 {
         void show() {
             System.out.println("show in Innerclass1");
         }
@@ -14,5 +14,7 @@ public class Inner6 {
     public static void main(String[] args) {
         OuterClass1 OC1 = new OuterClass1();
         OC1.display();
+        OuterClass1.InnerClass1 IC1 = new OuterClass1.InnerClass1();
+        IC1.show();
     }
 }
