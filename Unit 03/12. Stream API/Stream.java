a435525b525e6b4dcfd556444f5c3484ecc48b11
karvind08 <karvind08@gmail.com>
2026-03-31 11:03:58 +0530

    31 March 2026

--- Unit 03/12. Stream API/Stream.java
+++ Unit 03/12. Stream API/Stream.java
@@ -3,8 +3,8 @@ import java.util.*;
 public class Stream {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(10, 2, 13, -4, 5);
-        list.stream().map(n -> n * 2).forEach(n -> System.out.print(n));
-        System.out.println("The original List: ");
-        list.forEach(n -> System.out.print(n));
+        list.stream().map(n -> n * 2).forEach(n -> System.out.print(n + " "));
+        System.out.println("\nThe original List: ");
+        list.forEach(n -> System.out.print(n + " "));
     }
 }
