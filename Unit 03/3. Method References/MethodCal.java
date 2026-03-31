9dc7e4379f6b15388272b7e1cc8a13bb6806af28
karvind08 <karvind08@gmail.com>
2026-03-31 15:46:28 +0530

    31 March 2026

--- Unit 03/3. Method References/MethodCal.java
+++ Unit 03/3. Method References/MethodCal.java
@@ -8,22 +8,22 @@ class Classic {
     }
 
     static double sub(double a, double b) {
-        return a + b;
+        return a - b;
     }
 
     static double mul(double a, double b) {
-        return a + b;
+        return a * b;
     }
 
     static double div(double a, double b) {
-        return a + b;
+        return a / b;
     }
 }
 
 public class MethodCal {
     public static void main(String[] args) {
-        Calc C1 = Classic::add;
-        double res = C1.display(10.3, 3.4);
+        Calc C1 = Classic::div;
+        double res = C1.display(20, 4);
         System.out.println(res);
     }
 }
