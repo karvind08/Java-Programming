0bc79ce943a511dcb3eeb5ccd1f9940ba1b1e46e
karvind08 <karvind08@gmail.com>
2026-03-31 12:06:47 +0530

    31 March 2026

--- Unit 03/12. Stream API/Stream8.java
+++ Unit 03/12. Stream API/Stream8.java
@@ -1,3 +1,13 @@
-public class Stream8 {
+import java.util.*;
 
+public class Stream8 {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 7, 3, 8, 2);
+        int r = list.stream()
+                .reduce(0, (n, i) -> n + i);
+
+        System.out.println("\nThe sum is: " + r);
+        System.out.println("\nOriginal List");
+        list.forEach(n -> System.out.print(n + " "));
+    }
 }
