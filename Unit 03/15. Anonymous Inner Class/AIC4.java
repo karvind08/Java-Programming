27c3ba36733f8576a3b681043b323a110cfac277
karvind08 <karvind08@gmail.com>
2026-03-31 17:48:30 +0530

    31 March 2026

--- Unit 03/15. Anonymous Inner Class/AIC4.java
+++ Unit 03/15. Anonymous Inner Class/AIC4.java
@@ -1,3 +1,14 @@
-public class AIC4 {
+abstract class Inner4 {
+    abstract void display();
+}
 
+public class AIC4 {
+    public static void main(String[] args) {
+        Inner4 C4 = new Inner4() {
+            void display() {
+                System.out.println("Anonymous");
+            }
+        };
+        C4.display();
+    }
 }
