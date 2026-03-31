781b2b145fb901716a9a97dc54f9c81280783f60
karvind08 <karvind08@gmail.com>
2026-03-31 15:28:36 +0530

    31 March 2026

--- Unit 03/3. Method References/Method4.java
+++ Unit 03/3. Method References/Method4.java
@@ -1,3 +1,19 @@
+interface Inter4 {
+    Class4 display(int a);
+}
+
+class Class4 {
+    int a;
+
+    Class4(int a) {
+        this.a = a;
+        System.out.println("The value of a: " + a);
+    }
+}
+
 public class Method4 {
-    
+    public static void main(String[] args) {
+        Inter4 I1 = Class4::new; // now matches no-arg constructor
+        I1.display(10);
+    }
 }
