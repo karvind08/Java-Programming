ae56552c1523305a72854762dd17acab986c1097
karvind08 <karvind08@gmail.com>
2026-03-31 11:25:38 +0530

    31 March 2026

---
+++ Unit 03/12. Stream API/Stream3.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Stream3 {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 7, 3, 8, 2);
+        List<Integer> res = list.stream()
+                .map(n -> n * 2)
+                .collect(Collectors.toList());
+        res.forEach(n -> System.out.print(n + " "));
+        System.out.println("\nOrinial List");
+        list.forEach(n -> System.out.print(n + " "));
+    }
+}
