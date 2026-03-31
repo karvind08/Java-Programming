d4f53b0f917c4796a5509a9217d45fc990572b4b
karvind08 <karvind08@gmail.com>
2026-03-31 14:52:48 +0530

    31 March 2026

--- Unit 03/3. Method References/Method.java
+++ Unit 03/3. Method References/Method.java
@@ -1,3 +1,17 @@
-public class Method {
+interface Inter {
+    void display();
+}
 
+class Class1 {
+    void show() {
+        System.out.println("Show in Class1");
+    }
+}
+
+public class Method {
+    public static void main(String[] args) {
+        Class1 C1 = new Class1();
+        Inter I1 = C1::show;
+        I1.display();
+    }
 }
