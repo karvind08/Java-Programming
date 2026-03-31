b288aaef9db3daf15eaae21f3e59108b3504a3ad
karvind08 <karvind08@gmail.com>
2026-03-31 15:08:43 +0530

    31 March 2026

--- Unit 03/3. Method References/Method3.java
+++ Unit 03/3. Method References/Method3.java
@@ -1,3 +1,16 @@
-public class Method3 {
+interface Inter3 {
+    Class3 display();
+}
 
+class Class3 {
+    Class3() {
+        System.out.println("Constructor");
+    }
+}
+
+public class Method3 {
+    public static void main(String[] args) {
+        Inter3 I3 = Class3::new;
+        I3.display();
+    }
 }
