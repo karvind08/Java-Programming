8578004cd9fbf73ec3f79098accbf8e7158a6bdb
karvind08 <karvind08@gmail.com>
2026-03-31 11:31:51 +0530

    31 March 2026

--- Unit 03/12. Stream API/Stream4.java
+++ Unit 03/12. Stream API/Stream4.java
@@ -1,3 +1,16 @@
-public class Stream4 {
+import java.util.*;
+import java.util.stream.Collectors;
 
+public class Stream4 {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 7, 3, 8, 2);
+        List<Integer> res = list.stream()
+                .filter(n -> n % 2 == 0)
+                .collect(Collectors.toList());
+
+        System.out.println("\nThe even numbers are: ");
+        res.forEach(n -> System.out.print(n + " "));
+        System.out.println("\nOriginal List");
+        list.forEach(n -> System.out.print(n + " "));
+    }
 }
