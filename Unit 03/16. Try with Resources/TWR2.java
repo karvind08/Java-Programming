1865e532849a9fbb961116861d1f6c1d8027a96a
karvind08 <karvind08@gmail.com>
2026-03-31 16:40:37 +0530

    31 March 2026

--- Unit 03/16. Try with Resources/TWR2.java
+++ Unit 03/16. Try with Resources/TWR2.java
@@ -1,3 +1,18 @@
-public class TWR2 {
+import java.util.Scanner;
 
+public class TWR2 {
+    public static void main(String[] args) {
+        Scanner S = null;
+        try {
+            S = new Scanner(System.in);
+            System.out.println("Enter the Number: ");
+            int a = S.nextInt();
+            System.out.println("The number is " + a);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        } finally {
+            S.close();
+        }
+
+    }
 }
