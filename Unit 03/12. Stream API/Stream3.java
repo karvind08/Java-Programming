78d6a93f0f67f520c9d2cdb46279763f81571227
karvind08 <karvind08@gmail.com>
2026-03-31 11:26:43 +0530

    31 March 2026

--- Unit 03/12. Stream API/Stream3.java
+++ Unit 03/12. Stream API/Stream3.java
@@ -5,7 +5,7 @@ public class Stream3 {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 7, 3, 8, 2);
         List<Integer> res = list.stream()
-                .map(n -> n * 2)
+                .filter(n -> n % 2 == 0)
                 .collect(Collectors.toList());
         res.forEach(n -> System.out.print(n + " "));
         System.out.println("\nOrinial List");
