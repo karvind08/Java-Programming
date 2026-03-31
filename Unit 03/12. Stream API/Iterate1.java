0a26c5d66c30ae8be8b4ae0477e65c376045f7ba
karvind08 <karvind08@gmail.com>
2026-03-31 12:27:36 +0530

    31 March 2026

--- Unit 03/12. Stream API/Iterate1.java
+++ Unit 03/12. Stream API/Iterate1.java
@@ -1,3 +1,9 @@
-public class Iterate1 {
+import java.util.*;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
+public class Iterate1 {
+    public static void main(String[] args) {
+        List<Integer> list = list.forEach(n -> System.out.print(n + " "));
+    }
 }
