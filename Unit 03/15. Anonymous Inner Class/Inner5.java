e71001618b6a6b5c237a297ced89a8eb2618b2fb
karvind08 <karvind08@gmail.com>
2026-03-31 18:02:12 +0530

    31 March 2026

---
+++ Unit 03/15. Anonymous Inner Class/Inner5.java
@@ -0,0 +1,18 @@
+class OuterClass {
+    void display() {
+        System.out.println("Display method in OuterClass");
+    }
+
+    class InnerClass {
+        void show() {
+            System.out.println("Show method in InnerClass");
+        }
+    }
+}
+
+public class Inner5 {
+    public static void main(String[] args) {
+        OuterClass OC1 = new OuterClass();
+        OC1.display();
+    }
+}
