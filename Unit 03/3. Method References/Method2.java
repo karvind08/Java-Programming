21ebddd97fc0e21dcc76ee84d4e432555cb427cb
karvind08 <karvind08@gmail.com>
2026-03-31 15:04:23 +0530

    31 March 2026

--- Unit 03/3. Method References/Method2.java
+++ Unit 03/3. Method References/Method2.java
@@ -1,3 +1,27 @@
-public class Method2 {
+interface Inter2 {
+    Class2 display();
+}
 
+class Class2 {
+    int a;
+
+    Class2() {
+
+    }
+
+    Class2(int a) {
+        this.a = a;
+    }
+
+    void show() {
+        System.out.println("The value of a" + a);
+    }
+}
+
+public class Method2 {
+    public static void main(String[] args) {
+        Inter2 I1 = Class2::new; // now matches no-arg constructor
+        I1.display();
+
+    }
 }
