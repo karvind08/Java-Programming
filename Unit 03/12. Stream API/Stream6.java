171f314f78fc9029414f67f58562d789e2514f9a
karvind08 <karvind08@gmail.com>
2026-03-31 11:41:04 +0530

    31 March 2026

--- Unit 03/12. Stream API/Stream6.java
+++ Unit 03/12. Stream API/Stream6.java
@@ -1,3 +1,17 @@
-public class Stream6 {
+import java.util.*;
+import java.util.stream.Collectors;
 
+public class Stream6 {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 7, 3, 8, 2);
+        List<Integer> res = list.stream()
+                .filter(n -> n % 2 != 0)
+                .sorted()
+                .collect(Collectors.toList());
+
+        System.out.println("\nThe odd numbers in sorted order are: ");
+        res.forEach(n -> System.out.print(n + " "));
+        System.out.println("\nOriginal List");
+        list.forEach(n -> System.out.print(n + " "));
+    }
 }
