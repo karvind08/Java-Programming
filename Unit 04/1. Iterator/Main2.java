1986002d1cf9e620cf51af6be6583017990649a8
karvind08 <karvind08@gmail.com>
2026-04-04 20:01:19 +0530

    4 April 2026

--- Unit 04/1. Iterator/Main2.java
+++ Unit 04/1. Iterator/Main2.java
@@ -1,3 +1,29 @@
-public class Main2 {
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.List;
 
+class CustomCollection implements Iterable<String> {
+    private List<String> items = new ArrayList<>();
+
+    public void add(String item) {
+        items.add(item);
+    }
+
+    @Override
+    public Iterator<String> iterator() {
+        return items.iterator();
+    }
+}
+
+public class Main2 {
+    public static void main(String[] args) {
+        CustomCollection collection = new CustomCollection();
+        collection.add("Apple");
+        collection.add("Banana");
+        collection.add("Cherry");
+
+        for (String fruit : collection) {
+            System.out.println(fruit);
+        }
+    }
 }
