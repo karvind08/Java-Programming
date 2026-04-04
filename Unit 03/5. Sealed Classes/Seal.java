1dc2c1c86289388eba82a2986a4e477bacfd82ee
karvind08 <karvind08@gmail.com>
2026-04-04 16:51:18 +0530

    4 April 2026

--- Unit 03/5. Sealed Classes/Seal.java
+++ Unit 03/5. Sealed Classes/Seal.java
@@ -1,3 +1,18 @@
-public class Seal {
+sealed class Test permits User {
+    void display() {
+        System.out.println("Display in sealed Test");
+    }
+}
 
+final class User extends Test {
+    void display() {
+        System.out.println("Display in User");
+    }
+}
+
+public class Seal {
+    public static void main(String[] args) {
+        User U1 = new User();
+        U1.display();
+    }
 }
