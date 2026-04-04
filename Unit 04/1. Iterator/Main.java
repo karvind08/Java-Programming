2c8e5d5573bfb804efd6e39321e658ca8b8ceec5
karvind08 <karvind08@gmail.com>
2026-04-04 19:52:19 +0530

    4 April 2026

--- Unit 04/1. Iterator/Main.java
+++ Unit 04/1. Iterator/Main.java
@@ -1,3 +1,10 @@
+import java.util.*;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        Iterable<Integer> numbers = Arrays.asList(1, 2, 3, 4);
+        for (Integer I : numbers) {
+            System.out.println(I);
+        }
+    }
 }
