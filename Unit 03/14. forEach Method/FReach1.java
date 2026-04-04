1e2d1d3fde4e7f99e2f68d3f191b80e0e13e3308
karvind08 <karvind08@gmail.com>
2026-04-04 16:32:00 +0530

    4 April 2026

--- Unit 03/14. forEach Method/FReach1.java
+++ Unit 03/14. forEach Method/FReach1.java
@@ -1,3 +1,10 @@
-public class FReach1 {
+import java.util.*;
+import java.util.function.Consumer;
 
+public class FReach1 {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        Consumer<Integer> C1 = (Integer n) -> System.out.print(n + " ");
+        list.forEach(C1);
+    }
 }
