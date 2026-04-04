eaed75440466fb1239742baa8371d66c747c4037
karvind08 <karvind08@gmail.com>
2026-04-04 15:31:25 +0530

    4 April 2026

---
+++ Unit 03/18. Generic Classes/Wrapper1.java
@@ -0,0 +1,9 @@
+public class Wrapper1 {
+    public static void main(String[] args) {
+        Integer I1 = 10;
+        int a = I1.intValue();
+        System.out.println(a);
+        int b = I1;
+        System.out.println(b);
+    }
+}
