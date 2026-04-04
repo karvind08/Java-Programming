97689fa9a59b10bbb75f62ba4157b50b1157be2c
karvind08 <karvind08@gmail.com>
2026-04-04 15:14:59 +0530

    4 April 2026

---
+++ Unit 03/18. Generic Classes/Main3.java
@@ -0,0 +1,15 @@
+class Outer1 {
+    void display() {
+        System.out.println("Outer");
+    }
+
+    static class Inner1 {
+        void show() {
+            System.out.println("Inner Class");
+        }
+    }
+}
+
+public class Main3 {
+
+}
