f42da60e0d0d438273e9e4cab2c0a859e2663f4d
karvind08 <karvind08@gmail.com>
2026-04-04 21:42:51 +0530

    4 April 2026

--- Unit 04/1. Iterator/Main3.java
+++ Unit 04/1. Iterator/Main3.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+import java.util.*;
+
 public class Main3 {
     public static void main(String[] args) {
-        
+        ArrayList<String> colors = new ArrayList<>();
+        colors.add("Red");
+        colors.add("Blue");
+        colors.add("Green");
+        colors.add("Black");
+        colors.add("White");
+        Iterator<String> I1 = colors.iterator();
+        while (I1.hasNext()) {
+            System.out.println(I1.next());
+        }
     }
 }
