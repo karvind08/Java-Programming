6777a647cc2e1c214f6efc3f4d33e64915bf01c1
karvind08 <karvind08@gmail.com>
2026-04-04 15:27:06 +0530

    4 April 2026

---
+++ Unit 03/18. Generic Classes/Wrapper.java
@@ -0,0 +1,9 @@
+public class Wrapper {
+    public static void main(String[] args) {
+        int a = 10;
+        Integer A = Integer.valueOf(a);
+        System.out.println(a + " " + A);
+        Integer A1 = 20;
+        System.out.println(A1);
+    }
+}
