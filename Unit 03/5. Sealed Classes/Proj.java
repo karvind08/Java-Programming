4bb7a5dcbd44aa1c8ac7c342f2fb7cdfed006316
karvind08 <karvind08@gmail.com>
2026-04-04 18:26:27 +0530

    4 April 2026

--- Unit 03/5. Sealed Classes/Proj.java
+++ Unit 03/5. Sealed Classes/Proj.java
@@ -14,9 +14,41 @@ final class Circle1 extends Shape1 {
     }
 }
 
+final class Rectangle1 extends Shape1 {
+    int l, b;
+
+    Rectangle1(int l, int b) {
+        this.l = l;
+        this.b = b;
+    }
+
+    public double area() {
+        return l * b;
+    }
+}
+
+final class Square1 extends Shape1 {
+    int n;
+
+    Square1(int n) {
+        this.n = n;
+    }
+
+    public double area() {
+        return n * n;
+    }
+}
 
 public class Proj {
     public static void main(String[] args) {
-
+        Circle1 C1 = new Circle1(2);
+        double r1 = C1.area();
+        System.out.println(r1);
+        Rectangle1 R1 = new Rectangle1(2, 3);
+        double r2 = R1.area();
+        System.out.println(r2);
+        Square1 S1 = new Square1(5);
+        double r3 = S1.area();
+        System.out.println(r3);
     }
 }
