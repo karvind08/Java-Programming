fcd36e06079688c98e2863b4523ba15d9ca40b6c
karvind08 <karvind08@gmail.com>
2026-04-04 16:08:40 +0530

    4 April 2026

--- Unit 03/18. Generic Classes/Anonymous.java
+++ Unit 03/18. Generic Classes/Anonymous.java
@@ -6,6 +6,8 @@ class Test<T> {
 
 public class Anonymous {
     public static void main(String[] args) {
-
+        Test<Integer> T1 = new Test<>();
+        int res = T1.display(10);
+        System.out.println(res);
     }
 }
