66bc09089e44fb93256f8851bd18fc3a66f7c15a
karvind08 <karvind08@gmail.com>
2026-04-04 16:44:50 +0530

    4 April 2026

---
+++ Unit 03/14. forEach Method/FReach3.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class FReach3 {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(10, 20, 30);
+        list.forEach((Integer n) -> System.out.println(n));
+    }
+}
