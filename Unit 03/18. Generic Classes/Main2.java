673ee028eec1092eb7170d4a6af003be3563d45c
karvind08 <karvind08@gmail.com>
2026-04-04 15:13:25 +0530

    4 April 2026

--- Unit 03/18. Generic Classes/Main2.java
+++ Unit 03/18. Generic Classes/Main2.java
@@ -14,5 +14,8 @@ public class Main2 {
     public static void main(String[] args) {
         OuterClass A1 = new OuterClass();
         A1.display();
+        OuterClass.InnerClass B1 = A1.new InnerClass();
+        B1.show();
+
     }
 }
