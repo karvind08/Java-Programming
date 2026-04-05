f005bfea2f3a6e3212cd08523f1cc890a96092a2
karvind08 <karvind08@gmail.com>
2026-04-05 22:30:03 +0530

    5 April 2026

--- Unit 03/8. Record/Sample.java
+++ Unit 03/8. Record/Sample.java
@@ -6,6 +6,20 @@ class Employee {
         this.eid = eid;
         this.name = name;
     }
+
+    public int getEid() {
+        return eid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee [eid=" + eid + ", name=" + name + "]";
+    }
+
 }
 
 public class Sample {
