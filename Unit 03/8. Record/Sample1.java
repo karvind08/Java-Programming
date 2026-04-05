bfcc822233928346cccd977780ee1d87d042298b
karvind08 <karvind08@gmail.com>
2026-04-05 22:36:06 +0530

    5 April 2026

---
+++ Unit 03/8. Record/Sample1.java
@@ -0,0 +1,10 @@
+record Employee1(int eid, String name) {
+}
+
+public class Sample1 {
+    public static void main(String[] args) {
+        Employee1 E1 = new Employee1(10, "Yuvaan");
+        System.out.println(E1);
+
+    }
+}
