2b34f44633abb151272544a9ccce0de95b90fec2
karvind08 <karvind08@gmail.com>
2026-04-06 16:35:32 +0530

    6 April 2026

--- Unit 03/18. Generic Classes/Sample.java
+++ Unit 03/18. Generic Classes/Sample.java
@@ -1,3 +1,20 @@
-public class Sample {
+class SampleExample<T> {
+    private T a;
+    private T b;
 
+    public SampleExample(T a, T b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    void display() {
+        System.out.println(a + " " + b);
+    }
+}
+
+public class Sample {
+    public static void main(String[] args) {
+        SampleExample<Integer> S = new SampleExample<>(10, 20);
+        S.display();
+    }
 }
