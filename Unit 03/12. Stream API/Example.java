e7a119b4b92e7823be6ca6f7c0029a1279701eeb
karvind08 <karvind08@gmail.com>
2026-04-06 12:43:52 +0530

    6 April 2026

--- Unit 03/12. Stream API/Example.java
+++ Unit 03/12. Stream API/Example.java
@@ -1,3 +1,12 @@
-public class Example {
+import java.util.*;
+import java.util.stream.Stream;
 
+public class Example {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        System.out.println("The list is: " + list);
+        Stream<Integer> data = list.stream();
+        long res = data.count();
+        System.out.println("The elements are: " + res);
+    }
 }
