8aa210a68155bada8d9743b8ca328b2ee363b293
karvind08 <karvind08@gmail.com>
2026-04-06 16:40:52 +0530

    6 April 2026

--- Unit 03/18. Generic Classes/Sample1.java
+++ Unit 03/18. Generic Classes/Sample1.java
@@ -1,3 +1,22 @@
-public class Sample1 {
+class SampleExample1<R1, R2> {
+    private R1 a;
+    private R2 b;
 
+    void getdata(R1 a, R2 b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    void display() {
+        System.out.println(a + " " + b);
+    }
+
+}
+
+public class Sample1 {
+    public static void main(String[] args) {
+        SampleExample1<Integer, String> S1 = new SampleExample1<>();
+        S1.getdata(10, "Java");
+        S1.display();
+    }
 }
