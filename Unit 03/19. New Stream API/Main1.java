379181858158bc4f10d07f058d7c3676d71a6a1c
karvind08 <karvind08@gmail.com>
2026-04-07 21:56:41 +0530

    7 April 2026

--- Unit 03/19. New Stream API/Main1.java
+++ Unit 03/19. New Stream API/Main1.java
@@ -1,3 +1,9 @@
-public class Main1 {
+import java.util.*;
 
+public class Main1 {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, -3, 4, 5);
+        System.out.println("List: " + list);
+        list.stream().map(n -> n * n).forEach(n -> System.out.println(n));
+    }
 }
