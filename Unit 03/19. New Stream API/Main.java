200319dc4cfb26495b5be432382ab46054ed707d
karvind08 <karvind08@gmail.com>
2026-04-07 21:50:45 +0530

    7 April 2026

--- Unit 03/19. New Stream API/Main.java
+++ Unit 03/19. New Stream API/Main.java
@@ -3,7 +3,7 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> list = Arrays.asList(1, 22, -3, 4, 15);
         System.out.println("List: " + list);
         Stream<Integer> S = list.stream();
         Stream<Integer> S1 = S.sorted();
