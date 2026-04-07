1c999a8be2987a7854ddd26dfc809a54ab6d8f43
karvind08 <karvind08@gmail.com>
2026-04-07 22:06:42 +0530

    7 April 2026

--- Unit 03/19. New Stream API/Main2.java
+++ Unit 03/19. New Stream API/Main2.java
@@ -1,3 +1,14 @@
-public class Main2 {
+import java.util.*;
+import java.util.stream.Collectors;
+// import java.util.stream.Stream;
 
+public class Main2 {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, -3, 4, 5);
+        System.out.println("List: " + list);
+        List<Integer> res = list.stream()
+                .map(n -> n * n)
+                .collect(Collectors.toList());
+        System.out.println(res);
+    }
 }
