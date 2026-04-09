b08ca3ce83c0ea63a124a8a49e56ade24af98481
karvind08 <karvind08@gmail.com>
2026-04-09 22:27:11 +0530

    9 April 2026

---
+++ Unit 03/19. New Stream API/Peek.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Peek {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Arvind", "Amit", "Jiya");
+
+        names.stream()
+                .filter(name -> name.startsWith("A"))
+                .peek(name -> System.out.println("Filtered: " + name))
+                .map(String::toUpperCase)
+                .peek(name -> System.out.println("Mapped: " + name))
+                .forEach(System.out::println);
+    }
+}
