a6d999ab663bfd28a06e4d8cbdee161fa559c1af
karvind08 <karvind08@gmail.com>
2026-04-09 22:22:31 +0530

    9 April 2026

--- Unit 03/19. New Stream API/Distinct.java
+++ Unit 03/19. New Stream API/Distinct.java
@@ -1,3 +1,13 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Distinct {
-    
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Arvind", "Amit", "Arvind", "Jiya", "Amit");
+        List<String> uniqueNames = names.stream()
+                .distinct()
+                .collect(Collectors.toList());
+
+        System.out.println(uniqueNames);
+    }
 }
