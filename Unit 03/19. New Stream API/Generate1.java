90e765b814e9894be67878fab0597e60bdc8c565
karvind08 <karvind08@gmail.com>
2026-04-09 21:46:55 +0530

    9 April 2026

--- Unit 03/19. New Stream API/Generate1.java
+++ Unit 03/19. New Stream API/Generate1.java
@@ -1,3 +1,12 @@
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.*;
+
 public class Generate1 {
-    
+    public static void main(String[] args) {
+        List<String> names = Stream.generate(() -> "Arvind")
+                .limit(10)
+                .collect(Collectors.toList());
+        System.out.println(names);
+    }
 }
