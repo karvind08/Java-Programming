1449796d47b97d868dde0a7dbdc990154856a208
karvind08 <karvind08@gmail.com>
2026-04-09 21:44:22 +0530

    9 April 2026

--- Unit 03/19. New Stream API/Generate.java
+++ Unit 03/19. New Stream API/Generate.java
@@ -1,3 +1,12 @@
-public class Generate {
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.*;
 
+public class Generate {
+    public static void main(String[] args) {
+        List<Integer> res = Stream.generate(() -> 10)
+                .limit(10)
+                .collect(Collectors.toList());
+        System.out.println(res);
+    }
 }
