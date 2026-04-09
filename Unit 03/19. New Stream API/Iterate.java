c67aee51c5125bf2368064177e2fd0167a61f8ac
karvind08 <karvind08@gmail.com>
2026-04-09 21:39:03 +0530

    9 April 2026

--- Unit 03/19. New Stream API/Iterate.java
+++ Unit 03/19. New Stream API/Iterate.java
@@ -1,3 +1,15 @@
-public class Iterate {
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+public class Iterate {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        System.out.println(list);
+        List<Integer> res = Stream.iterate(1, i -> i + 1)
+                .limit(10)
+                .collect(Collectors.toList());
+        System.out.println(res);
+    }
 }
