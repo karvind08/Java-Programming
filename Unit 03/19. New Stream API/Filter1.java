5627f70ecabb6f1543e4179cc5d49fca37dfd9bb
karvind08 <karvind08@gmail.com>
2026-04-09 21:35:10 +0530

    9 April 2026

--- Unit 03/19. New Stream API/Filter1.java
+++ Unit 03/19. New Stream API/Filter1.java
@@ -1,3 +1,13 @@
-public class Filter1 {
+import java.util.*;
 
+public class Filter1 {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        System.out.println(list);
+        int res = list.stream()
+                .filter(n -> n % 2 == 0)
+                .reduce(0, (n, i) -> n + i);
+        System.out.println(res);
+
+    }
 }
