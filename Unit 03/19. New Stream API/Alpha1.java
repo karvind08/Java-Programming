e98c41fca4fe7920e3d9e81b3047d2384518fea1
karvind08 <karvind08@gmail.com>
2026-04-09 21:59:34 +0530

    9 April 2026

--- Unit 03/19. New Stream API/Alpha1.java
+++ Unit 03/19. New Stream API/Alpha1.java
@@ -1,3 +1,13 @@
-public class Alpha1 {
+import java.util.*;
+import java.util.stream.Collectors;
 
+public class Alpha1 {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Arvind", "Yuvaan", "Amit", "Jiya");
+        List<String> res = names.stream()
+                .filter(name -> name.startsWith("A"))
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
+        System.out.println(res);
+    }
 }
