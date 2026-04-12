ef17f21f620becd617cbf50f749ddb1230cde0d3
karvind08 <karvind08@gmail.com>
2026-04-12 15:43:27 +0530

    12 April 2026

--- Unit 04/Queue/Main2.java
+++ Unit 04/Queue/Main2.java
@@ -1,3 +1,14 @@
-public class Main2 {
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class Main2 {
+    public static void main(String[] args) {
+        Queue<Integer> Q1 = new LinkedList<>();
+        Q1.add(10);
+        Q1.add(20);
+        Q1.add(30);
+        System.out.println(Q1);
+        Q1.remove(20);
+        System.out.println("After removing 20: " + Q1);
+    }
 }
