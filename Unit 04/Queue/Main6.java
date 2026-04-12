017ba029db890f4caedc63e8674ed5183a5f1e83
karvind08 <karvind08@gmail.com>
2026-04-12 16:12:36 +0530

    12 April 2026

--- Unit 04/Queue/Main6.java
+++ Unit 04/Queue/Main6.java
@@ -1,3 +1,17 @@
-public class Main6 {
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class Main6 {
+    public static void main(String[] args) {
+        Queue<String> Q1 = new LinkedList<>();
+        Q1.add("C");
+        Q1.add("C++");
+        Q1.add("Java");
+        System.out.println(Q1);
+        System.out.println(Q1.size());
+        Iterator<String> itr = Q1.iterator();
+        while (itr.hasNext())
+            System.out.println(itr.next());
+    }
 }
