76609df6aa4602316b347d21c329ab20931b9a3a
karvind08 <karvind08@gmail.com>
2026-04-12 15:53:43 +0530

    12 April 2026

--- Unit 04/Queue/Main4.java
+++ Unit 04/Queue/Main4.java
@@ -1,3 +1,15 @@
-public class Main4 {
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class Main4 {
+    public static void main(String[] args) {
+        Queue<String> Q1 = new LinkedList<>();
+        Q1.add("C");
+        Q1.add("C++");
+        Q1.add("Java");
+        System.out.println(Q1);
+        System.out.println("Head using peek: " + Q1.peek());
+        System.out.println("Head using peek: " + Q1.element());
+        System.out.println("After operations: " + Q1);
+    }
 }
