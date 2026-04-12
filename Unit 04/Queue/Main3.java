a10e8f88b66d34a87756d23dfc6b8da6dac2b601
karvind08 <karvind08@gmail.com>
2026-04-12 15:46:22 +0530

    12 April 2026

---
+++ Unit 04/Queue/Main3.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main3 {
+    public static void main(String[] args) {
+        Queue<Integer> Q1 = new LinkedList<>();
+        Q1.add(10);
+        Q1.add(20);
+        Q1.add(30);
+        System.out.println(Q1);
+        Q1.poll();
+        System.out.println("After polling 20: " + Q1);
+    }
+}
