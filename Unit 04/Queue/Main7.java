0a418ee4799f29057784f968260adc94c57e4d5a
karvind08 <karvind08@gmail.com>
2026-04-12 16:37:39 +0530

    12 April 2026

--- Unit 04/Queue/Main7.java
+++ Unit 04/Queue/Main7.java
@@ -1,3 +1,12 @@
-public class Main7 {
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class Main7 {
+    public static void main(String[] args) {
+        Queue<String> Q1 = new LinkedList<>();
+        Q1.add("C");
+        Q1.add("C++");
+        Q1.add("Java");
+        System.out.println(Q1);
+    }
 }
