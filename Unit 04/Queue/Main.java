dd6b20ee166ebc7a156551bdc4204b26def2fb19
karvind08 <karvind08@gmail.com>
2026-04-12 15:36:18 +0530

    12 April 2026

---
+++ Unit 04/Queue/Main.java
@@ -0,0 +1,12 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) {
+        Queue<Integer> Q1 = new PriorityQueue<>();
+        Q1.add(10);
+        Q1.add(20);
+        Q1.add(30);
+        System.out.println(Q1);
+    }
+}
