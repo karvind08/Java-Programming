07ac8ac78da5d7417a5e67e552aed1eab12d67ef
karvind08 <karvind08@gmail.com>
2026-04-13 22:28:49 +0530

    13 April 2026

--- Unit 04/1. Iterable/Parallel.java
+++ Unit 04/1. Iterable/Parallel.java
@@ -1,3 +1,11 @@
-public class Parallel {
+import java.util.*;
 
+public class Parallel {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+
+        list.parallelStream()
+                .forEach(n -> System.out.println(
+                        Thread.currentThread().getName() + " processed " + n));
+    }
 }
