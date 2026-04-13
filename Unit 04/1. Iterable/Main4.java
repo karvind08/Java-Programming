3dc1954e2fc4f6d0f09cdd0ff2db9122dfd39a8a
karvind08 <karvind08@gmail.com>
2026-04-13 22:01:36 +0530

    13 April 2026

--- Unit 04/1. Iterable/Main4.java
+++ Unit 04/1. Iterable/Main4.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Spliterator;
 
 public class Main4 {
     public static void main(String[] args) {
@@ -7,5 +8,7 @@ public class Main4 {
         list.add(20);
         list.add(30);
         System.out.println(list);
+        Spliterator<Integer> sit = list.spliterator();
+
     }
 }
