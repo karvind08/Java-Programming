6b99c7d7097195faa264258104d1e25a17c12edb
karvind08 <karvind08@gmail.com>
2026-04-13 21:59:17 +0530

    13 April 2026

--- Unit 04/1. Iterable/Main4.java
+++ Unit 04/1. Iterable/Main4.java
@@ -1,3 +1,11 @@
-public class Main4 {
+import java.util.ArrayList;
 
+public class Main4 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        System.out.println(list);
+    }
 }
