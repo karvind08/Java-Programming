b4b571a8cd546845166cb8ef97aded9bd7858e09
karvind08 <karvind08@gmail.com>
2026-04-13 22:13:42 +0530

    13 April 2026

--- Unit 04/1. Iterable/Main5.java
+++ Unit 04/1. Iterable/Main5.java
@@ -1,3 +1,15 @@
-public class Main5 {
+import java.util.*;
 
+public class Main5 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        System.out.println(list);
+        Spliterator<Integer> sit = list.spliterator();
+        sit.tryAdvance(System.out::println);
+        System.out.println("Remaining data: ");
+        sit.forEachRemaining(System.out::println);
+    }
 }
