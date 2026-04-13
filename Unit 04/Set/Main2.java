9dbe97e18ce2aedf68d2c05f0344fed4b252bcf7
karvind08 <karvind08@gmail.com>
2026-04-13 22:50:17 +0530

    13 April 2026

--- Unit 04/Set/Main2.java
+++ Unit 04/Set/Main2.java
@@ -1,3 +1,21 @@
-public class Main2 {
+import java.util.HashSet;
+import java.util.Set;
 
+public class Main2 {
+    public static void main(String[] args) {
+        Set<Integer> set1 = new HashSet<>();
+        set1.add(2);
+        set1.add(3);
+        System.out.println("Set1: " + set1);
+        Set<Integer> set2 = new HashSet<>();
+        set2.add(1);
+        set2.add(2);
+        System.out.println("Set2: " + set2);
+        // Union of two sets
+        set1.containsAll(set2);
+        System.out.println("Subset: " + set1);
+        set2.containsAll(set1);
+        System.out.println("Intersection: " + set2);
+
+    }
 }
