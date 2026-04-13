c6cfc9b02a4ae548edd0492a754a2123c1a32073
karvind08 <karvind08@gmail.com>
2026-04-13 22:40:33 +0530

    13 April 2026

--- Unit 04/Set/Main.java
+++ Unit 04/Set/Main.java
@@ -1,3 +1,11 @@
-public class Main {
+import java.util.HashSet;
+import java.util.Set;
 
+public class Main {
+    public static void main(String[] args) {
+        Set<Integer> set1 = new HashSet<>();
+        set1.add(2);
+        set1.add(3);
+        System.out.println("Set1: " + set1);
+    }
 }
