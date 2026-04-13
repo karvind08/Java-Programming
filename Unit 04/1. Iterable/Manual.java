06c8ba98d18f967d0dfb76df4a2b78d9edba543e
karvind08 <karvind08@gmail.com>
2026-04-13 22:23:39 +0530

    13 April 2026

--- Unit 04/1. Iterable/Manual.java
+++ Unit 04/1. Iterable/Manual.java
@@ -1,3 +1,15 @@
-public class Manual {
+import java.util.*;
 
+public class Manual {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+        Spliterator<Integer> spl1 = list.spliterator();
+        Spliterator<Integer> spl2 = spl1.trySplit(); // split into two parts
+
+        System.out.println("First half:");
+        spl1.forEachRemaining(System.out::println);
+
+        System.out.println("Second half:");
+        spl2.forEachRemaining(System.out::println);
+    }
 }
