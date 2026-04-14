6496c9cd13c27e2f4ca40b2a7494f79f14cb22fa
karvind08 <karvind08@gmail.com>
2026-04-14 17:36:08 +0530

    14 April 2026

---
+++ Unit 04/3. List/Main3.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main3 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(11);
+        list.add(2);
+        list.add(5);
+        list.add(3);
+    }
+}
