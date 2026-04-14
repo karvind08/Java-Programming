7e6e6963bf5fb76d29bc92173ad6aee645c4179b
karvind08 <karvind08@gmail.com>
2026-04-14 19:59:10 +0530

    14 April 2026

--- Unit 04/4. Comparable Interface/Main.java
+++ Unit 04/4. Comparable Interface/Main.java
@@ -1,3 +1,16 @@
+class Employee {
+    int eid;
+    String name;
+    double sal;
+
+    Employee(int eid, String name, double sal) {
+        this.eid = eid;
+        this.name = name;
+        this.sal = sal;
+    }
+
+}
+
 public class Main {
     public static void main(String[] args) {
 
