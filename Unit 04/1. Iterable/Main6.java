2df44a54c4bde581880f0fc860f12e9c5b03223c
karvind08 <karvind08@gmail.com>
2026-04-14 14:34:22 +0530

    14 April 2026

--- Unit 04/1. Iterable/Main6.java
+++ Unit 04/1. Iterable/Main6.java
@@ -1,3 +1,10 @@
-public class Main6 {
+import java.util.*;
 
+public class Main6 {
+    public static void main(String[] args) {
+        Iterable<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        for (Integer i : list) {
+            System.out.println(i);
+        }
+    }
 }
