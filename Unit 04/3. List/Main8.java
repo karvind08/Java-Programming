25329bc6a6f7f90a0d1c83cd7010d9ff01263210
karvind08 <karvind08@gmail.com>
2026-04-14 18:11:08 +0530

    14 April 2026

--- Unit 04/3. List/Main8.java
+++ Unit 04/3. List/Main8.java
@@ -1,3 +1,10 @@
-public class Main8 {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Main8 {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        System.out.println(list.isEmpty());
+
+    }
 }
