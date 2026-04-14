17bb181196b02722b5c352e191bcbab0925f46d5
karvind08 <karvind08@gmail.com>
2026-04-14 14:55:44 +0530

    14 April 2026

--- Unit 04/1. Iterable/Main9.java
+++ Unit 04/1. Iterable/Main9.java
@@ -1,3 +1,17 @@
-public class Main9 {
+import java.util.ArrayList;
+import java.util.Spliterator;
 
+public class Main9 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        Spliterator<Integer> SI = list.spliterator();
+        SI.tryAdvance(n -> System.out.println(n));
+        System.out.println("Remaining: ");
+        SI.forEachRemaining(n -> System.out.println(n));
+    }
 }
