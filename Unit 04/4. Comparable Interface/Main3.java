01ac11148ade6fddb017295017b6baa3d2922b76
karvind08 <karvind08@gmail.com>
2026-04-14 20:59:45 +0530

    14 April 2026

--- Unit 04/4. Comparable Interface/Main3.java
+++ Unit 04/4. Comparable Interface/Main3.java
@@ -1,8 +1,16 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 
 public class Main3 {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
+        list.add(90);
+        list.add(9);
+        list.add(19);
+        list.add(3);
+        list.add(6);
+        Collections.sort(list);
+        System.out.println(list);
     }
 }
