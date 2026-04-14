babdb76952fdbd7fea0bf9885b27ca24a484d5ea
karvind08 <karvind08@gmail.com>
2026-04-14 16:41:34 +0530

    14 April 2026

--- Unit 04/3. List/Main.java
+++ Unit 04/3. List/Main.java
@@ -1,3 +1,15 @@
-public class Main {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        list.add(null);
+        list.forEach(System.out::print);
+    }
 }
