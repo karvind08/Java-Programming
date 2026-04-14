a4ef1af1b66f967c60a6ff075e4b5c10016f7539
karvind08 <karvind08@gmail.com>
2026-04-14 17:59:05 +0530

    14 April 2026

--- Unit 04/3. List/Main6.java
+++ Unit 04/3. List/Main6.java
@@ -1,3 +1,15 @@
-public class Main6 {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Main6 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(1);
+        list.add(100);
+        list.add(20);
+        System.out.println(list);
+        list.set(4, 30);
+        System.out.println("The updated List: " + list);
+    }
 }
