3034164aa239d3f855f8c37f59e1c200fb815219
karvind08 <karvind08@gmail.com>
2026-04-14 15:01:20 +0530

    14 April 2026

--- Unit 04/1. Iterable/Main10.java
+++ Unit 04/1. Iterable/Main10.java
@@ -11,7 +11,9 @@ public class Main10 {
         list.add(50);
         Spliterator<Integer> SP1 = list.spliterator();
         Spliterator<Integer> SP2 = SP1.trySplit();
+        SP1.tryAdvance(System.out::println);
         SP1.forEachRemaining(n -> System.out.print(n + " "));
+        SP2.tryAdvance(System.out::println);
         System.out.println("\nSecond Half: ");
         SP2.forEachRemaining(n -> System.out.print(n + " "));
     }
