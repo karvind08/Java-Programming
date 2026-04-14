a5037d1e71a691abdf9eeffea88d71a2d1bc1d2c
karvind08 <karvind08@gmail.com>
2026-04-14 17:25:23 +0530

    14 April 2026

--- Unit 04/3. List/Main2.java
+++ Unit 04/3. List/Main2.java
@@ -1,3 +1,10 @@
-public class Main2 {
+import java.util.*;
 
+public class Main2 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(5);
+    }
 }
