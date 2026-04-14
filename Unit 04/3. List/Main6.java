7d2b4e08dd3a55c6c45b9c48288583ef966ef7b8
karvind08 <karvind08@gmail.com>
2026-04-14 17:59:26 +0530

    14 April 2026

--- Unit 04/3. List/Main6.java
+++ Unit 04/3. List/Main6.java
@@ -8,8 +8,5 @@ public class Main6 {
         list.add(1);
         list.add(100);
         list.add(20);
-        System.out.println(list);
-        list.set(4, 30);
-        System.out.println("The updated List: " + list);
     }
 }
