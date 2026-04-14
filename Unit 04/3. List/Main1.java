c4b7da7e99383c516d3c1e328d736719719872f6
karvind08 <karvind08@gmail.com>
2026-04-14 16:55:57 +0530

    14 April 2026

--- Unit 04/3. List/Main1.java
+++ Unit 04/3. List/Main1.java
@@ -4,12 +4,12 @@ import java.util.List;
 public class Main1 {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
-        list.add(10);
-        list.add(20);
-        list.add(30);
-        list.add(40);
-        list.add(50);
-        list.add(null);
+        // list.add(10);
+        // list.add(20);
+        // list.add(30);
+        // list.add(40);
+        // list.add(50);
+        // list.add(null);
         System.out.println(list);
     }
 }
