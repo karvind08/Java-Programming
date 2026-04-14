74cce12117db3f317e2300300dd3e8eac02527c8
karvind08 <karvind08@gmail.com>
2026-04-14 16:26:54 +0530

    14 April 2026

--- Unit 04/2. Collection/Main1.java
+++ Unit 04/2. Collection/Main1.java
@@ -1,3 +1,21 @@
-public class Main1 {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 
+public class Main1 {
+    public static void main(String[] args) {
+        Collection<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        for (Integer i : list)
+            System.out.println(i);
+        list.remove(10);
+        // list.forEach(n -> System.out.println(n));
+        Iterator<Integer> I1 = list.iterator();
+        while (I1.hasNext()) {
+            System.out.println(I1.next());
+        }
+    }
 }
