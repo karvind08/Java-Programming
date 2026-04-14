e9f4c52d667f2ff6afc2f935138817bba3dd2461
karvind08 <karvind08@gmail.com>
2026-04-14 14:59:32 +0530

    14 April 2026

---
+++ Unit 04/1. Iterable/Main10.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Spliterator;
+
+public class Main10 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        Spliterator<Integer> SP1 = list.spliterator();
+        Spliterator<Integer> SP2 = SP1.trySplit();
+        SP1.forEachRemaining(n -> System.out.print(n));
+        System.out.println("Second Half: ");
+        SP2.forEachRemaining(n -> System.out.print(n));
+    }
+}
