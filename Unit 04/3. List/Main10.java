05cf7f0b1ba6b7fd99c6090feebb046eb8eed71b
karvind08 <karvind08@gmail.com>
2026-04-14 18:25:07 +0530

    14 April 2026

--- Unit 04/3. List/Main10.java
+++ Unit 04/3. List/Main10.java
@@ -1,3 +1,35 @@
-public class Main10 {
+class Student {
+    int r;
+    String name;
+    double h;
 
+    Student(int r, String name, double h) {
+        this.r = r;
+        this.name = name;
+        this.h = h;
+    }
+
+    public int getR() {
+        return r;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getH() {
+        return h;
+    }
+
+    @Override
+    public String toString() {
+        return r + " " + name + " " + h + " ";
+    }
+
+}
+
+public class Main10 {
+    public static void main(String[] args) {
+        
+    }
 }
