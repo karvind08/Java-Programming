623bb0b60f854ce13f90c5b9fc0d7fe29773e01d
karvind08 <karvind08@gmail.com>
2026-04-14 14:39:30 +0530

    14 April 2026

--- Unit 04/1. Iterable/Main7.java
+++ Unit 04/1. Iterable/Main7.java
@@ -1,3 +1,16 @@
-public class Main7 {
+import java.util.ArrayList;
+import java.util.Iterator;
 
+public class Main7 {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Arvind");
+        list.add("Yuvaan");
+        list.add("Jiya");
+        list.add("Duggu");
+        Iterator<String> itr = list.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+    }
 }
