63a258af31160448a6402b5d495d163ce3f6e460
karvind08 <karvind08@gmail.com>
2026-04-14 21:07:18 +0530

    14 April 2026

--- Unit 04/4. Comparable Interface/Main4.java
+++ Unit 04/4. Comparable Interface/Main4.java
@@ -1,3 +1,24 @@
-public class Main4 {
+import java.util.ArrayList;
+import java.util.List;
 
+class Student {
+    int r;
+    String name;
+
+    Student(int r, String name) {
+        this.r = r;
+        this.name = name;
+    }
+}
+
+public class Main4 {
+    public static void main(String[] args) {
+        List<Student> list = new ArrayList<>();
+        list.add(new Student(1, "Bhanu"));
+        list.add(new Student(2, "Arvind"));
+        for (Student i : list) {
+            System.out.println(i.r + " " + i.name);
+        }
+
+    }
 }
