de89750ee096bd4f866996e18152749c5fed8190
karvind08 <karvind08@gmail.com>
2026-04-14 18:15:20 +0530

    14 April 2026

--- Unit 04/3. List/Main9.java
+++ Unit 04/3. List/Main9.java
@@ -1,3 +1,13 @@
-public class Main9 {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Main9 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(1);
+        list.add(100);
+        list.add(1);
+        System.out.println(list.contains(10));
+    }
 }
