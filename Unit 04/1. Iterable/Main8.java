75dad0f25c0ed3060c3e90a3d932d1c940963753
karvind08 <karvind08@gmail.com>
2026-04-14 14:42:22 +0530

    14 April 2026

--- Unit 04/1. Iterable/Main8.java
+++ Unit 04/1. Iterable/Main8.java
@@ -1,3 +1,15 @@
-public class Main8 {
+import java.util.ArrayList;
+import java.util.Iterator;
 
+public class Main8 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        Iterator<Integer> itr = list.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+    }
 }
