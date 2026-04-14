8c27837900a7a51a1dc15c7798ed4efe04a6ab47
karvind08 <karvind08@gmail.com>
2026-04-14 18:08:28 +0530

    14 April 2026

--- Unit 04/3. List/Main7.java
+++ Unit 04/3. List/Main7.java
@@ -1,3 +1,14 @@
-public class Main7 {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Main7 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(1);
+        list.add(100);
+        list.add(1);
+        int ind = list.get(1);
+        System.out.println(ind);
+    }
 }
