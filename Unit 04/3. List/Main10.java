e97c2e3444e9611870f328bf9b3621f56c064609
karvind08 <karvind08@gmail.com>
2026-04-14 18:32:38 +0530

    14 April 2026

--- Unit 04/3. List/Main10.java
+++ Unit 04/3. List/Main10.java
@@ -12,18 +12,6 @@ class Student {
         this.h = h;
     }
 
-    public int getR() {
-        return r;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public double getH() {
-        return h;
-    }
-
     @Override
     public String toString() {
         return r + " " + name + " " + h + " ";
@@ -36,6 +24,9 @@ public class Main10 {
         List<Student> list = new ArrayList<>();
         list.add(new Student(1, "Arvind", 5.8d));
         list.add(new Student(2, "Yuvaan", 2.5d));
-        System.out.println(list);
+        for (Student s : list) {
+            System.out.println(s);
+        }
+
     }
 }
