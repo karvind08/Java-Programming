2fb82b84699939ba0d7e367953566617700cfd5b
karvind08 <karvind08@gmail.com>
2026-04-14 16:21:19 +0530

    14 April 2026

--- Unit 04/2. Collection/Main.java
+++ Unit 04/2. Collection/Main.java
@@ -1,3 +1,13 @@
-public class Main {
+import java.util.ArrayList;
+import java.util.Collection;
 
+public class Main {
+    public static void main(String[] args) {
+        Collection<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        
+    }
 }
