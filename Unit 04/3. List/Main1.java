125d2a2adaf248641deb88fcf208bc605485d833
karvind08 <karvind08@gmail.com>
2026-04-14 16:55:14 +0530

    14 April 2026

---
+++ Unit 04/3. List/Main1.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Main1 {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        list.add(null);
+        System.out.println(list);
+    }
+}
