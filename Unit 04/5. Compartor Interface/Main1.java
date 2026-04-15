5ec95a4b3650e1730997c46da2b5d6bcae81df1b
karvind08 <karvind08@gmail.com>
2026-04-15 21:11:52 +0530

    15 April 2026

---
+++ Unit 04/5. Compartor Interface/Main1.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+class Student1 {
+    int r, age;
+    String name;
+
+    Student1(int r, int age, String name) {
+        this.r = r;
+        this.name = name;
+        this.age = age;
+    }
+}
+
+class SortbyAge implements Comparator<Student1> {
+    public int compare(Student1 S1, Student1 S2) {
+        return S2.age - S1.age;
+    }
+}
+
+class SortbyName implements Comparator<Student1> {
+    public int compare(Student1 S1, Student1 S2) {
+        return S1.name.compareTo(S2.name);
+    }
+}
+
+public class Main1 {
+    public static void main(String[] args) {
+        List<Student1> list = new ArrayList<>();
+        list.add(new Student1(1, 10, "Bhanu"));
+        list.add(new Student1(4, 22, "Arvind"));
+        list.add(new Student1(10, 18, "Chandu"));
+        list.add(new Student1(9, 30, "Sandeep"));
+        Collections.sort(list, new SortbyAge());
+        for (Student1 i : list) {
+            System.out.println(i.r + " " + i.age + " " + i.name);
+        }
+
+    }
+}
