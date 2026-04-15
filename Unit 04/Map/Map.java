3f8471e2cc2b2f46c5d8816ed8e368d859a6ea2d
karvind08 <karvind08@gmail.com>
2026-04-15 22:18:51 +0530

    15 April 2026

--- Unit 04/Map/Map.java
+++ Unit 04/Map/Map.java
@@ -1,3 +1,11 @@
-public class Map {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Map {
+    public static void main(String[] args) {
+        Map<String, Integer> M = new HashMap<>();
+        M.put("One", 1);
+        M.put("Two", 2);
+        System.out.println(M);
+    }
 }
