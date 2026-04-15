6ceeb9c51bd071e562d1da25cdbfed31c36f93e3
karvind08 <karvind08@gmail.com>
2026-04-15 22:25:13 +0530

    15 April 2026

--- Unit 04/Map/Map1.java
+++ Unit 04/Map/Map1.java
@@ -1,9 +1,9 @@
-import java.util.Map; // <-- important
+import java.util.Map;
 import java.util.HashMap;
 
 public class Map1 {
     public static void main(String[] args) {
-        Map<String, Integer> M = new HashMap<String, Integer>();
+        Map<String, Integer> M = new HashMap<>();
         M.put("One", 1);
         M.put("Two", 2);
         System.out.println(M);
