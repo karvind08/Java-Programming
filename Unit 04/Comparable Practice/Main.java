cc47c4acc8a8d6a8f6a875120dea6f5c29c482a5
karvind08 <karvind08@gmail.com>
2026-04-16 21:06:37 +0530

    16 April 2026

---
+++ Unit 04/Comparable Practice/Main.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.*;
+
+class Employee implements Comparable<Employee> {
+    int eid;
+    String name;
+    double sal;
+
+    Employee(int eid, String name, double sal) {
+        this.eid = eid;
+        this.name = name;
+        this.sal = sal;
+    }
+
+    // public int compareTo(Employee E) {
+    // if (sal == E.sal)
+    // return 0;
+    // else if (sal > E.sal)
+    // return 1;
+    // else
+    // return -1;
+    // }
+
+    public int compareTo(Employee E) {
+        return this.eid - E.eid;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        List<Employee> list = new ArrayList<>();
+        list.add(new Employee(6, "Arvind", 50000));
+        list.add(new Employee(12, "Yuvaan", 20000));
+        list.add(new Employee(1, "Jiya`", 30000));
+        Collections.sort(list);
+        for (Employee e : list) {
+            System.out.println(e.eid + " " + e.name + " " + e.sal);
+        }
+    }
+}
