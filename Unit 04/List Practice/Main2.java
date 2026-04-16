1626b1f75b7e756f6d6e9508c7e99022b6c3ccb3
karvind08 <karvind08@gmail.com>
2026-04-16 20:39:18 +0530

    16 April 2026

--- Unit 04/List Practice/Main2.java
+++ Unit 04/List Practice/Main2.java
@@ -1,3 +1,12 @@
-public class Main2 {
+import java.util.*;
 
+public class Main2 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(12);
+        list.add(23);
+        list.add(2);
+        System.out.println("original list: " + list);
+        System.out.println(list.contains(2));
+    }
 }
