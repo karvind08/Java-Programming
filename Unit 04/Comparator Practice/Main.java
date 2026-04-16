97eaef1271096c3b889ab67c4ed407c9b6a5493b
karvind08 <karvind08@gmail.com>
2026-04-16 22:14:14 +0530

    16 April 2026

--- Unit 04/Comparator Practice/Main.java
+++ Unit 04/Comparator Practice/Main.java
@@ -16,13 +16,19 @@ class SortbyRoll implements Comparator<Student> {
     }
 }
 
+class SortbyName implements Comparator<Student> {
+    public int compare(Student S1, Student S2) {
+        return S1.name.compareTo(S2.name);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         List<Student> list = new ArrayList<>();
         list.add(new Student(4, "Bhavya"));
         list.add(new Student(3, "Chavya"));
         list.add(new Student(1, "Arvind"));
-        Collections.sort(list, new SortbyRoll());
+        Collections.sort(list, new SortbyName());
         for (Student s : list) {
             System.out.println(s.roll + " " + s.name);
         }
