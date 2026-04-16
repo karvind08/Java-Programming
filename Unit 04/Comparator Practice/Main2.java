ede34907e7e30a33e10c65ef4f5c58ba8ad02eba
karvind08 <karvind08@gmail.com>
2026-04-16 22:17:34 +0530

    16 April 2026

--- Unit 04/Comparator Practice/Main2.java
+++ Unit 04/Comparator Practice/Main2.java
@@ -1,3 +1,26 @@
-public class Main2 {
+import java.util.*;
 
+class Student {
+    int roll;
+    String name;
+
+    Student(int roll, String name) {
+        this.roll = roll;
+        this.name = name;
+    }
+}
+
+public class Main2 {
+    public static void main(String[] args) {
+        List<Student> list = new ArrayList<>();
+        list.add(new Student(4, "Bhavya"));
+        list.add(new Student(3, "Chavya"));
+        list.add(new Student(1, "Arvind"));
+        // Collections.sort(list, (S1, S2) -> S1.roll - S2.roll);
+
+        for (Student s : list) {
+            System.out.println(s.roll + " " + s.name);
+        }
+
+    }
 }
