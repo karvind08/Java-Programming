24ed474b2b4ae8a43ab1a14e8a8ea903bacfe439
karvind08 <karvind08@gmail.com>
2026-04-16 20:15:12 +0530

    16 April 2026

---
+++ Unit 04/List Practice/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(12);
+        list.add(23);
+        list.add(2);
+        list.sort(Comparator.naturalOrder());
+        System.out.println(list);
+        // Collections.sort(list);
+
+    }
+}
