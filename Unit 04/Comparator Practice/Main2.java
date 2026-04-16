6049e98015c4b8c2f431ac9be266884533b8bce7
karvind08 <karvind08@gmail.com>
2026-04-16 22:19:55 +0530

    16 April 2026

--- Unit 04/Comparator Practice/Main2.java
+++ Unit 04/Comparator Practice/Main2.java
@@ -16,8 +16,7 @@ public class Main2 {
         list.add(new Student(4, "Bhavya"));
         list.add(new Student(3, "Chavya"));
         list.add(new Student(1, "Arvind"));
-        // Collections.sort(list, (S1, S2) -> S1.roll - S2.roll);
-
+        list.sort(Comparator.comparing(S -> S.name));
         for (Student s : list) {
             System.out.println(s.roll + " " + s.name);
         }
