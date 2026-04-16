53c3487f38b1f610b5dfb41f75329af604481132
karvind08 <karvind08@gmail.com>
2026-04-16 21:07:05 +0530

    16 April 2026

--- Unit 04/Comparable Practice/Main.java
+++ Unit 04/Comparable Practice/Main.java
@@ -32,7 +32,7 @@ public class Main {
         List<Employee> list = new ArrayList<>();
         list.add(new Employee(6, "Arvind", 50000));
         list.add(new Employee(12, "Yuvaan", 20000));
-        list.add(new Employee(1, "Jiya`", 30000));
+        list.add(new Employee(1, "Jiya", 30000));
         Collections.sort(list);
         for (Employee e : list) {
             System.out.println(e.eid + " " + e.name + " " + e.sal);
