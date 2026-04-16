c892081a1cea30c33868d4f2addac45a0e9ba8d9
karvind08 <karvind08@gmail.com>
2026-04-16 22:51:30 +0530

    15 April 2026

--- Unit 04/Comparator Practice/Main3.java
+++ Unit 04/Comparator Practice/Main3.java
@@ -1,3 +1,34 @@
-public class Main3 {
+import java.util.*;
 
+class Student1 {
+    int r;
+    String name;
+
+    public int getR() {
+        return r;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    Student1(int r, String name) {
+        this.r = r;
+        this.name = name;
+    }
+
+}
+
+public class Main3 {
+    public static void main(String[] args) {
+        List<Student1> list = new ArrayList<>();
+        list.add(new Student1(3, "Bob"));
+        list.add(new Student1(9, "Arvind"));
+        list.add(new Student1(30, "Yuvaan"));
+        list.sort(Comparator.comparingInt(Student1::getR)
+                .thenComparing(Student1::getName));
+        for (Student1 s : list) {
+            System.out.println(s.r + " " + s.name);
+        }
+    }
 }
