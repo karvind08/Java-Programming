3d9910614a9f807ff0e2d8cb161f959bef19e75b
karvind08 <karvind08@gmail.com>
2026-04-19 22:55:31 +0530

    19 April 2026

--- Unit 05/spring project 1/src/main/java/com/arvind/spring/Hello.java
+++ Unit 05/spring project 1/src/main/java/com/arvind/spring/Hello.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Hello {
     @RequestMapping("/")
     public String display() {
-        return "Hello World";
+        return "Hello World, Welcome to Spring !";
     }
 }
