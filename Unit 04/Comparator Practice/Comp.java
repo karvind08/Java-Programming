c02b28c7ac8ee6404e6e4f3cc9cf5d093d9fe4ec
karvind08 <karvind08@gmail.com>
2026-04-19 22:22:33 +0530

    19 April 2026

--- Unit 04/Comparator Practice/Comp.java
+++ Unit 04/Comparator Practice/Comp.java
@@ -1,3 +1,16 @@
+class Student3 {
+    int age;
+    String name;
+    double marks;
+
+    Student3(int age, String name, double marks) {
+        this.age = age;
+        this.name = name;
+        this.marks = marks;
+    }
+
+}
+
 public class Comp {
-    
+
 }
