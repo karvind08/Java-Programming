4cb35686de6cc9cc76b898ca4981954081910519
karvind08 <karvind08@gmail.com>
2026-04-19 22:30:31 +0530

    19 April 2026

--- Unit 04/Comparator Practice/Comp.java
+++ Unit 04/Comparator Practice/Comp.java
@@ -21,7 +21,8 @@ public class Comp {
         list.add(new Student3(9, "Arvind", 64.50));
         // list.sort(Comparator.comparingInt(S -> S.age));
         // list.sort(Comparator.comparing(S -> S.name));
-        list.sort(Comparator.comparingDouble(S -> S.marks));
+        // 
+        
         for (Student3 i : list) {
             System.out.println(i.age + " " + i.name + " " + i.marks);
         }
