4adc1ec42477d134f909a618fb2dbc593470eab4
karvind08 <karvind08@gmail.com>
2026-04-20 22:58:53 +0530

    20 April 2026

--- Unit 04/HashSet/Main.java
+++ Unit 04/HashSet/Main.java
@@ -3,6 +3,10 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         HashSet<Integer> hs = new HashSet<>();
+        hs.add(10);
+        hs.add(10);
+        hs.add(20);
+        hs.add(null);
         System.out.println(hs);
     }
 }
