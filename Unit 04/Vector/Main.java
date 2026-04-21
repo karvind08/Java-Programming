14453ae160bd7d39b20bbe7229c45ddf0b3c5dca
karvind08 <karvind08@gmail.com>
2026-04-21 09:42:01 +0530

    21 April 2026

---
+++ Unit 04/Vector/Main.java
@@ -0,0 +1,11 @@
+import java.util.Vector;
+
+public class Main {
+    public static void main(String[] args) {
+        Vector<Integer> v = new Vector<>();
+        System.out.println(v.size());
+        v.add(10);
+        System.out.println(v);
+        System.out.println(v.size());
+    }
+}
